01ddec4ea03551c4ed4a062a68bd2d908d336869
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-07-10 16:01:09 +0300

    YTM-9 header for single issue

--- src/views/single-issue/singe-issue.js
+++ src/views/single-issue/singe-issue.js
@@ -2,14 +2,33 @@ var React = require('react-native');
 var {
     StyleSheet,
     Text,
-    View
+    View,
+    TouchableHighlight
     } = React;
 
+let issueListStyles = require('../issue-list/issue-list.styles');
+
 class SingeIssueView extends React.Component {
+
+    _renderHeader() {
+        return (
+            <View style={issueListStyles.headerContainer}>
+                <TouchableHighlight
+                    underlayColor="#FFF"
+                    style={issueListStyles.logOut}
+                    onPress={() => this.props.onBack()}>
+                    <Text style={issueListStyles.logOut__text}>List</Text>
+                </TouchableHighlight>
+
+                <Text style={styles.headerText}>{this.props.issueId}</Text>
+            </View>
+        )
+    }
 
     render() {
         return (
             <View style={styles.container}>
+                {this._renderHeader()}
                 <Text>
                     {this.props.issueId}
                 </Text>
@@ -20,9 +39,12 @@ class SingeIssueView extends React.Component {
 
 var styles = StyleSheet.create({
     container: {
-        paddingTop: 20,
         flex: 1,
         backgroundColor: '#F5FCFF'
+    },
+    headerText: {
+        top: 11,
+        left: 145
     }
 });
 
