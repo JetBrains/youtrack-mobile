167eedc71a74bff1aeafcb9b605a4bbafde37db7
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-07-29 00:55:49 +0300

    Refresh token

--- src/blocks/auth/auth__oauth.js
+++ src/blocks/auth/auth__oauth.js
@@ -5,7 +5,7 @@ var shittyQs = require('shitty-qs');
 function openAuthPage() {
     LinkingIOS.openURL([
         `${config.auth.serverUri}/auth/login`,
-        '?response_type=token',
+        '?response_type=code',
         `&client_id=${config.auth.clientId}`,
         `&scope=${config.auth.scopes}`,
         `&redirect_uri=${config.auth.landingUrl}`
@@ -16,8 +16,24 @@ function hubOAuth() {
     return new Promise(function (resolve, reject) {
 
         LinkingIOS.addEventListener('url', function (event) {
-            var [, query_string] = event.url.match(/\#(.*)/);
-            resolve(shittyQs(query_string));
+            var [, query_string] = event.url.match(/\?(.*)/);
+            let code = shittyQs(query_string).code;
+
+            fetch([
+                `http://hackathon15.labs.intellij.net:8080/hub/auth/token`,
+                '?grant_type=authorization_code',
+                `&client_id=${config.auth.clientId}`,
+                `&scope=${config.auth.scopes}`,
+                `&code=${code}`,
+                `&redirect_uri=${config.auth.landingUrl}`
+            ].join(''), {
+                //method: 'POST'
+            }).then(res => {
+                debugger;
+            }).catch(err => {
+                debugger;
+            });
+            //resolve(shittyQs(query_string));
         });
 
         openAuthPage();
