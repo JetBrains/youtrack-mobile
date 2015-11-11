2867f061a7c943bd686e63ba5a775121ca1715b8
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-11-11 11:14:58 +0300

    Use hub.jetbrains.com

--- src/blocks/app/app__config.js
+++ src/blocks/app/app__config.js
@@ -1,6 +1,6 @@
 module.exports = {
     auth: {
-        serverUri: 'https://sso.jetbrains.com',
+        serverUri: 'https://hub.jetbrains.com',
         clientId: '567ae9c4-3064-4d82-9370-425d0a6e5ad4',
         clientSecret: 'UFPqCa7VqHK3',
         scopes: 'Hub YouTrack',
