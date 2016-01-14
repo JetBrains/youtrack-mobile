84201a04c9c4ee847e1f8b3fabad0457f2afd334
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-14 16:29:10 +0300

    Tried to set up jest tests

---
+++ __tests__/api.test.js
@@ -0,0 +1,7 @@
+var ApiHelper = require('../src/blocks/api/api__helper');
+
+describe('ApiHelper', function() {
+    it('should import ApiHelper', function() {
+        expect(ApiHelper).toBeDefined();
+    });
+});
