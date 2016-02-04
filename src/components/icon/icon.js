d1e3d64d0e35e3e3984f48c77b19a959f2c32255
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-02-04 11:32:37 +0300

    YTM-5 add icons

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,2 +1,10 @@
 export const logo = require('./youtrack-logo.png');
 export const arrow = require('./arrow.png');
+export const add = require('./add.png');
+export const attach = require('./attach.png');
+export const back = require('./back.png');
+export const link = require('./link.png');
+export const next = require('./next.png');
+export const search = require('./search.png');
+export const tag = require('./tag.png');
+export const visible = require('./visible.png');
