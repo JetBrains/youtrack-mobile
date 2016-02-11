42f1cec5ded97f4b5579644c90b7b9929166d3b5
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-02-11 12:39:28 +0300

    YTM-16 initial layout for create view

--- src/views/create-issue/create-issue.js
+++ src/views/create-issue/create-issue.js
@@ -1,11 +1,45 @@
-import React, {View, Text} from 'react-native'
+import React, {View, Text, TextInput} from 'react-native'
 import styles from './create-issue.styles';
+import Header from '../../components/header/header';
 
 export default class CreateIssue extends React.Component {
+    createIssue() {
+
+    }
+
+    _focusOnSummary() {
+        
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello</Text>
+                <Header leftButton={<Text>Cancel</Text>}
+                        rightButton={<Text>Create</Text>}
+                        onRightButtonClick={this.createIssue.bind(this)}>
+                    <Text>New Issue</Text>
+                </Header>
+                <View>
+                    <View>
+                        <TextInput
+                            autoFocus={true}
+                            style={styles.summaryInput}
+                            placeholder="Summary"
+                            returnKeyType="next"
+                            onSubmitEditing={() => this._focusOnSummary()}
+                            onChangeText={(summary) => this.setState({summary})}/>
+                    </View>
+                    <View style={styles.separator}/>
+                    <View>
+                        <TextInput
+                            autoFocus={true}
+                            style={styles.descriptionInput}
+                            multiline={true}
+                            placeholder="Description"
+                            onChangeText={(description) => this.setState({description})}/>
+                    </View>
+                    <View style={styles.separator}/>
+                </View>
             </View>
         );
     }
--- src/views/create-issue/create-issue.styles.js
+++ src/views/create-issue/create-issue.styles.js
@@ -1,8 +1,29 @@
 import {StyleSheet} from 'react-native';
-import {UNIT} from '../../components/variables/variables';
+import {UNIT, COLOR_PINK} from '../../components/variables/variables';
 
 export default StyleSheet.create({
     container: {
-
+        flex: 1
+    },
+    summaryInput: {
+        minHeight: UNIT*6,
+        margin: UNIT,
+        padding: UNIT,
+        color: '#7E7E84',
+        fontSize: 18
+    },
+    descriptionInput: {
+        minHeight: UNIT*6,
+        margin: UNIT,
+        padding: UNIT,
+        flex: 1,
+        backgroundColor: '#FFF',
+        color: '#7E7E84',
+        borderColor: 'black',
+        fontSize: 14
+    },
+    separator: {
+        height: 0.5,
+        backgroundColor: '#C8C7CC'
     }
 });
