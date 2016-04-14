e2be7eebcbe12d201d41fd5de07d13f6f8dcb406
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-04-14 20:46:35 +0300

    Increase touch zone for header buttons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,8 @@ import React, {Text, View, TouchableOpacity, PropTypes} from 'react-native';
 import styles from './header.styles';
 import Router from '../router/router';
 
+const TOUCH_PADDING = 8;
+
 export default class Header extends React.Component {
   onBack() {
     if (this.props.onBack) {
@@ -19,6 +21,7 @@ export default class Header extends React.Component {
   render() {
     return (<View style={styles.header}>
       <TouchableOpacity
+        hitSlop={{top: TOUCH_PADDING, left: TOUCH_PADDING, bottom: TOUCH_PADDING, right: TOUCH_PADDING}}
         style={styles.headerButton}
         onPress={() => this.onBack()}>
         <Text style={styles.headerButtonText}>{this.props.leftButton}</Text>
@@ -27,6 +30,7 @@ export default class Header extends React.Component {
       <View style={styles.headerCenter}>{this.props.children}</View>
 
       <TouchableOpacity
+        hitSlop={{top: TOUCH_PADDING, left: TOUCH_PADDING, bottom: TOUCH_PADDING, right: TOUCH_PADDING}}
         style={styles.headerButton}
         onPress={this.onRightButtonClick.bind(this)}>
         <Text style={styles.headerButtonText}>{this.props.rightButton}</Text>
