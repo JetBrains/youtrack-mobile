b64820055508f174cd98d49c970b5d4b0d0ead34
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-05-05 23:45:40 +0300

    Implement download&extract script for teamcity build iOS archive

--- tools/get-ios-build-number.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-BuildNumber=$(curl https://buildserver.labs.intellij.net/guestAuth/app/rest/9.0/builds/?locator=buildType:YouTrack_YouTrackMobile_Develop_IOS,count:1,status:SUCCESS  -silent --show-error -H "Accept: application/json" | grep 'number' | grep -o -E '[0-9]+')
-
-echo $BuildNumber
