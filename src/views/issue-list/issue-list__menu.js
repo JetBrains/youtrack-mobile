02f2ded72e3fae1782689cb24826da4a6a3a9fd6
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-05-05 20:47:50 +0300

    Use build from package config

--- src/views/issue-list/issue-list__menu.js
+++ src/views/issue-list/issue-list__menu.js
@@ -4,7 +4,8 @@ import styles from './issue-list__menu.styles';
 const CURRENT_YEAR = (new Date()).getFullYear();
 
 const VERSION = process.env.npm_package_version;
-const BUILD_NUMBER = process.env.BUILD_NUMBER;
+const BUILD_NUMBER = process.env.npm_package_config_build;
+const VERSION_STRING = `${VERSION}-${BUILD_NUMBER}`;
 
 export default class IssueListMenu extends React.Component {
   render() {
@@ -14,7 +15,7 @@ export default class IssueListMenu extends React.Component {
       </TouchableOpacity>
 
       <View style={styles.menuFooter}>
-        <Text style={styles.footerText}>YouTrack Mobile {VERSION}{BUILD_NUMBER ? `-${BUILD_NUMBER}` : ''}</Text>
+        <Text style={styles.footerText}>YouTrack Mobile {VERSION_STRING}` : ''}</Text>
         <Text style={styles.footerText}>© 2000—{CURRENT_YEAR} JetBrains</Text>
         <Text style={styles.footerText}>All rights reserved</Text>
       </View>
