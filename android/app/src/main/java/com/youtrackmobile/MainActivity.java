08b02831aaaeeda35ba0137ed1a0fc72af57de91
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-05-17 12:42:51 +0300

    YTM-72 revert main component name change

--- android/app/src/main/java/com/youtrackmobile/MainActivity.java
+++ android/app/src/main/java/com/youtrackmobile/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "YouTrack";
+        return "YouTrackMobile";
     }
 
     /**
