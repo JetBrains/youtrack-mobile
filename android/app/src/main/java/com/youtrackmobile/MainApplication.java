b759a9435222435cdb00fd800778019d2411d16e
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-07-08 17:00:37 +0300

    Drop wrong mImagePicker initialization

--- android/app/src/main/java/com/youtrackmobile/MainApplication.java
+++ android/app/src/main/java/com/youtrackmobile/MainApplication.java
@@ -23,8 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      mImagePicker = new ImagePickerPackage(this);
-
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
             new ImagePickerPackage());
