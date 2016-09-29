a0dee4e0fc62099c2b43d999eea28dc029a0abdb
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-09-29 12:12:56 +0300

    YTM-47 pass GA ID from outside

--- src/components/usage/usage.js
+++ src/components/usage/usage.js
@@ -4,20 +4,29 @@ import {Analytics, Hits as GAHits} from 'react-native-google-analytics';
 
 const VERSION = process.env.npm_package_version;
 const BUILD_NUMBER = process.env.npm_package_config_buildnumber;
+const VERSTION_STRING = `${VERSION || 'dev'}-${BUILD_NUMBER || 'dev'}`;
+
+const googleAnalyiticsId =  process.env.npm_package_config_analyticsid;
+const isAnalyticsEnabled = googleAnalyiticsId !== null;
 
 const clientId = DeviceInfo.getUniqueID();
 
-//todo: pass GA ID in build step
-const version = `${VERSION || 'dev'}-${BUILD_NUMBER || 'dev'}`;
-
-const ga = new Analytics('UA-60566164-3', clientId, 1, DeviceInfo.getUserAgent());
+const ga = new Analytics(googleAnalyiticsId, clientId, 1, DeviceInfo.getUserAgent());
 
 const usage = {
   trackScreenView(screenName: string) {
-    const screenView = new GAHits.ScreenView('YouTrack Mobile', screenName, version);
+    if (!isAnalyticsEnabled) {
+      return;
+    }
+
+    const screenView = new GAHits.ScreenView('YouTrack Mobile', screenName, VERSTION_STRING);
     return ga.send(screenView);
   },
+
   trackEvent(eventName: string, ...params: Array<any>) {
+    if (!isAnalyticsEnabled) {
+      return;
+    }
     const gaEvent = new GAHits.Event(eventName, ...params);
     return ga.send(gaEvent);
   }
