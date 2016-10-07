eec4b8d793fa1a40605fd8b89e4a2c927cec948a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-10-07 15:37:32 +0300

    YTM-119 use react-native-gallery for images showing to allow zoom

--- src/views/show-image/show-image.js
+++ src/views/show-image/show-image.js
@@ -1,41 +1,33 @@
-import {StyleSheet, Image, View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Router from '../../components/router/router';
+import Gallery from 'react-native-gallery';
 
 const TOUCH_PADDING = 12;
 
-class ShowImage extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollView} maximumZoomScale={20}>
-          <View style={{flex: 1}}>
-            <Image style={styles.image} source={{uri: this.props.imageUrl}}/>
-          </View>
-        </ScrollView>
+export function ShowImage(props) {
+  return (
+    <View style={styles.container}>
+      <Gallery
+        style={{flex: 1, backgroundColor: 'black'}}
+        images={[props.imageUrl]}
+        initialPage={0}
+      />
 
-        <TouchableOpacity style={styles.closeButton}
-                          onPress={() => Router.pop()}
-                          hitSlop={{top: TOUCH_PADDING, left: TOUCH_PADDING, bottom: TOUCH_PADDING, right: TOUCH_PADDING}}>
-          <Text>Close</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+      <TouchableOpacity style={styles.closeButton}
+                        onPress={() => Router.pop()}
+                        hitSlop={{top: TOUCH_PADDING, left: TOUCH_PADDING, bottom: TOUCH_PADDING, right: TOUCH_PADDING}}>
+        <Text>Close</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#FFF'
-  },
-  scrollView: {
     flex: 1
   },
-  image: {
-    resizeMode: 'contain',
-    flex: 1
-  },
+
   closeButton: {
     position: 'absolute',
     backgroundColor: '#FFF7',
