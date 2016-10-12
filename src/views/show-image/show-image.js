9975fcbb2d7c5e5940611ffa3c19a13b2097f660
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-10-12 18:26:59 +0300

    Add @flow to show-image.js

--- src/views/show-image/show-image.js
+++ src/views/show-image/show-image.js
@@ -1,3 +1,4 @@
+/* @flow */
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Router from '../../components/router/router';
@@ -5,7 +6,12 @@ import Gallery from 'react-native-gallery';
 
 const TOUCH_PADDING = 12;
 
-export function ShowImage(props) {
+type Props = {
+  allImagesUrls: Array<string>,
+  currentImage: string
+}
+
+export function ShowImage(props: Props) {
 
   const currentIndex = props.allImagesUrls.indexOf(props.currentImage);
 
