70fcc42199aeeab57d34265b57dc9348833f2ecd
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-12-16 14:00:41 +0300

    YTM-270 add tests to date/integer fields render

--- src/components/custom-field/custom-field.test.js
+++ src/components/custom-field/custom-field.test.js
@@ -11,7 +11,10 @@ describe('<CustomField/>', () => {
       projectCustomField: {
         emptyFieldText: 'im empty',
         field: {
-          name: 'Test custom field'
+          name: 'Test custom field',
+          fieldType: {
+            valueType: 'some-type'
+          }
         }
       },
       value: {
@@ -37,6 +40,25 @@ describe('<CustomField/>', () => {
     const wrapper = shallow(<CustomField field={fakeField}/>);
     const value = wrapper.find({testID: 'value'});
     value.children().should.have.text(fakeField.value.name);
+  });
+
+  it('should render value of type date', () => {
+    const timestamp = 1481885918348;
+    fakeField.value = timestamp;
+    fakeField.projectCustomField.field.fieldType.valueType = 'date';
+
+    const wrapper = shallow(<CustomField field={fakeField}/>);
+    const value = wrapper.find({testID: 'value'});
+    value.children().should.have.text(new Date(timestamp).toLocaleDateString());
+  });
+
+    it('should render value of type integer', () => {
+    fakeField.value = 123;
+    fakeField.projectCustomField.field.fieldType.valueType = 'integer';
+
+    const wrapper = shallow(<CustomField field={fakeField}/>);
+    const value = wrapper.find({testID: 'value'});
+    value.children().should.have.text('123');
   });
 
   it('should render user field value', () => {
