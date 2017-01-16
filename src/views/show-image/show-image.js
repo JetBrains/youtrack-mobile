51b84c39c4d6ba647d1edcb301a8868f1540acc5
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-16 16:42:35 +0300

    YTM-286 YTM-242 make image gallery display loader and close by gestures

--- src/views/show-image/show-image.js
+++ src/views/show-image/show-image.js
@@ -1,10 +1,11 @@
 /* @flow */
-import {StyleSheet, View, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Image, TouchableOpacity, ActivityIndicator} from 'react-native';
 import React from 'react';
 import {closeOpaque} from '../../components/icon/icon';
 import Router from '../../components/router/router';
 import {UNIT} from '../../components/variables/variables';
 import Gallery from 'react-native-gallery';
+import once from 'lodash.once';
 
 const TOUCH_PADDING = 12;
 
@@ -18,19 +19,25 @@ type Props = {
 }
 
 export function ShowImage(props: Props) {
-
   const currentIndex = props.allImagesUrls.indexOf(props.currentImage);
+
+  const closeView = once(function closeView() {
+    return Router.pop();
+  });
 
   return (
     <View style={styles.container}>
       <Gallery
-        style={{flex: 1, backgroundColor: 'black'}}
+        style={styles.gallery}
         images={props.allImagesUrls}
         initialPage={currentIndex}
+        loader={<ActivityIndicator style={styles.loader} size="large"/>}
+        onSlideUp={closeView}
+        onSlideDown={closeView}
       />
 
       <TouchableOpacity style={styles.closeButton}
-                        onPress={() => Router.pop()}
+                        onPress={closeView}
                         hitSlop={hitSlop}>
         <Image style={styles.closeIcon} source={closeOpaque}></Image>
       </TouchableOpacity>
@@ -41,6 +48,19 @@ export function ShowImage(props: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+
+  gallery: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+
+  loader: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
 
   closeButton: {
