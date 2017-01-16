acefc8f1289651ea308d26c8cd21bd935675c98f
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-16 12:27:52 +0300

    Fix keystore usage

--- src/components/keystore/keystore.ios.js
+++ src/components/keystore/keystore.ios.js
@@ -1,12 +1,12 @@
 /* @flow */
-import NativeKeychain from 'react-native-keychain';
+import {getInternetCredentials, setInternetCredentials} from 'react-native-keychain';
 
 export default {
   getInternetCredentials(server: string) {
-    return NativeKeychain.getInternetCredentials(server);
+    return getInternetCredentials(server);
   },
 
   setInternetCredentials(server: string, username: string, password: string) {
-    return NativeKeychain.setInternetCredentials(server, username, password);
+    return setInternetCredentials(server, username, password);
   }
 };
