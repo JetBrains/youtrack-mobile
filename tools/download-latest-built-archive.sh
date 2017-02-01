9cf4a0c7c942aebbb625d621c44e5d4db2baa012
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-02-01 11:29:55 +0300

    Change download links to Develop branch

--- tools/download-latest-built-archive.sh
+++ tools/download-latest-built-archive.sh
@@ -6,13 +6,13 @@ echo "CI server: $1"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-BuildNumber=$(curl $1/guestAuth/app/rest/9.0/builds/?locator=buildType:YouTrack_YouTrackMobile_71_BuildNumber,count:1,status:SUCCESS --insecure -H "Accept: application/json" | grep 'number' | grep -o -E '[0-9]+')
+BuildNumber=$(curl $1/guestAuth/app/rest/9.0/builds/?locator=buildType:YouTrack_YouTrackMobile_Develop_BuildNumber,count:1,status:SUCCESS --insecure -H "Accept: application/json" | grep 'number' | grep -o -E '[0-9]+')
 
 echo ">>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
 echo Build number = $BuildNumber
 echo ">>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
 
-wget $1/guestAuth/repository/download/YouTrack_YouTrackMobile_71_Ios/$BuildNumber/YouTrackMobile[Release].zip -O artifacts.zip
+wget $1/guestAuth/repository/download/YouTrack_YouTrackMobile_Develop_IOS/$BuildNumber/YouTrackMobile[Release].zip -O artifacts.zip
 
 unzip -o artifacts.zip -d ../ios/build/YouTrackMobile[Release].xcarchive
 
