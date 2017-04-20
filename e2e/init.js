461ea5d01041b79751220f627892355b10be1ae4
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-20 21:57:09 +0300

    YTM-364 reliable connect&login e2e tests

--- e2e/init.js
+++ e2e/init.js
@@ -2,10 +2,16 @@ require('babel-polyfill');
 const detox = require('detox');
 const config = require('../package.json').detox;
 
-before(() => {
-  return detox.init(config);
+before(async () => {
+  await detox.init(config);
 });
 
-after(() => {
-  return detox.cleanup();
+beforeEach(async () => {
+  await device._fbsimctl._execFbsimctlCommand({args: `${device._simulatorUdid} clear_keychain`});
+  await device.relaunchApp({delete: true});
+});
+
+after(async () => {
+  await detox.cleanup();
+  // await device.shutdown();
 });
--- e2e/pages/login.js
+++ e2e/pages/login.js
@@ -6,17 +6,18 @@ const LOGIN_INPUT = 'login-input';
 const PASS_INPUT = 'password-input';
 
 module.exports = {
-  connectToServer: () => {
-    return element(by.id('server-url')).tap()
-      .then(() => element(by.id('server-url')).typeText(SERVER_URL))
-      .then(() => element(by.id('next')).tap())
+  connectToServer: async (serverUrl = SERVER_URL) => {
+    await element(by.id('server-url')).tap();
+    await element(by.id('server-url')).typeText(serverUrl);
+    await element(by.id('next')).tap();
   },
 
-  logIn: (login = DEFAULT_LOGIN, pass = DEFAULT_PASS) => {
-    return element(by.id(LOGIN_INPUT)).tap()
-      .then(() => element(by.id(LOGIN_INPUT)).replaceText(login))
-      .then(() => element(by.id(PASS_INPUT)).tap())
-      .then(() => element(by.id(PASS_INPUT)).replaceText(pass))
-      .then(() => element(by.id('log-in')).tap());
+  logIn: async (login = DEFAULT_LOGIN, pass = DEFAULT_PASS) => {
+    await element(by.id(LOGIN_INPUT)).tap();
+    await element(by.id(LOGIN_INPUT)).typeText(login);
+
+    await element(by.id(PASS_INPUT)).tap();
+    await element(by.id(PASS_INPUT)).typeText(pass);
+    await element(by.id('log-in')).tap();
   }
 };
