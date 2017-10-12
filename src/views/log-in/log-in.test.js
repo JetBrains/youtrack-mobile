40192ba28687bb8c6a8515c3d857fca6cb7c5ab9
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-10-12 21:12:02 +0300

    Add simple log-in form test

---
+++ src/views/log-in/log-in.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import LogIn from './log-in';
+import {shallow} from 'enzyme';
+import sinon from 'sinon';
+
+describe('LogIn', () => {
+  let defaultProps;
+
+  beforeEach(() => {
+    const fakeAuth = {
+      config: {
+        backendUrl: 'http://ytbackend',
+        auth: {
+          serverUri: 'http://hub'
+        }
+      },
+      authorizeCredentials: sinon.spy(),
+      authorizeOAuth: sinon.spy()
+    };
+
+    defaultProps = {
+      auth: fakeAuth,
+      onLogIn: sinon.spy(),
+      onChangeServerUrl: sinon.spy()
+    };
+
+    ReactNative.NativeModules.RNKeychainManager = {
+      getInternetCredentialsForServer: sinon.stub().returns(Promise.resolve({username: 'foo', password: 'bar'})),
+      setInternetCredentialsForServer: sinon.spy()
+    };
+  });
+
+  function shallowRender(props) {
+    return shallow(<LogIn {...defaultProps} {...props}/>);
+  }
+
+  it('should render', () => {
+    shallowRender({}).should.be.defined;
+  });
+
+  it('should log in with credentials', () => {
+    const wrapper = shallowRender({});
+
+    const instance = wrapper.instance();
+    instance.logInViaCredentials = sinon.spy();
+    instance.forceUpdate();
+
+    wrapper.find({testID: 'login-input'}).simulate('change', {target: {value: 'foo'}});
+    wrapper.find({testID: 'password-input'}).simulate('change', {target: {value: 'bar'}});
+    wrapper.find({testID: 'log-in'}).simulate('press');
+
+    instance.logInViaCredentials.should.have.been.called;
+  });
+});
