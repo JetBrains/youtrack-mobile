3f1efcd2d637180d21fdc5b3ac76367f775306fb
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-12-19 19:21:30 +0300

    YTM-507 fix bold markup highlight

--- src/components/wiki/wiki.js
+++ src/components/wiki/wiki.js
@@ -65,7 +65,7 @@ export default class Wiki extends Component<Props, void> {
 
   renderNode = (node: Object, index: number, siblings: any, parent: Object, defaultRenderer: (any, any) => any) => {
     const {imageHeaders, attachments} = this.props;
-
+console.log(node)
     if (node.type === 'text' && node.data === '\n') {
       return HTML_RENDER_NOTHING;
     }
@@ -79,6 +79,12 @@ export default class Wiki extends Component<Props, void> {
 
     if (node.name === 'img') {
       return renderImage({node, index, attachments, imageHeaders, onImagePress: this.onImagePress});
+    }
+
+    if (node.name === 'strong') {
+      return (
+        <Text key={index} style={{fontWeight: 'bold'}}>{defaultRenderer(node.children, parent)}</Text>
+      );
     }
 
     if (node.name === 'font') {
