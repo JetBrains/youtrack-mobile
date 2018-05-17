e5dd8f6459d67295118b2921c58a402007c1171f
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-05-17 16:06:02 +0300

    fixup! YTM-516 Added very simple test for create issue project ID loading

--- src/views/create-issue/create-issue.test.js
+++ src/views/create-issue/create-issue.test.js
@@ -4,30 +4,15 @@ import * as types from './create-issue-action-types';
 import {populateStorage, flushStoragePart} from '../../components/storage/storage';
 import sinon from 'sinon';
 
-const FAKE_DRAFT_ID = 'FAKE_DRAFT_ID';
 const FAKE_PROJECT_ID = 'FAKE_PROJECT_ID';
 
 describe('Issue creation actions', () => {
   let dispatch;
-  let getState;
-  let getApi;
   let sandbox;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
-    const state = {
-      app: {
-        auth: {
-          currentUser: {id: 'current-user'}
-        }
-      }
-    };
-    const api = {
-      loadIssueDraft: sinon.spy()
-    };
     dispatch = sinon.spy();
-    getState = () => state;
-    getApi = () => api;
   });
 
   afterEach(() => sandbox.restore());
