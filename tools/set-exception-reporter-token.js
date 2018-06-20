89478f21cf835d38a8c9dd4dce3b7bac580a881d
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-06-20 14:13:07 +0300

    YTM-565 fixup: exception reporter token field renamed

--- tools/set-exception-reporter-token.js
+++ tools/set-exception-reporter-token.js
@@ -1,14 +1,14 @@
 const updateJsonFile = require('update-json-file');
 
-const EXCEPTION_ROBOT_TOKEN = process.env.exeptionReporterToken;
+const EXCEPTION_REPORTER_TOKEN = process.env.exeptionReporterToken;
 
-if (!EXCEPTION_ROBOT_TOKEN) {
+if (!EXCEPTION_REPORTER_TOKEN) {
   console.error('process.env', process.env);
   throw new Error('exeptionReporterToken is not set');
 }
 
 updateJsonFile('package.json', data => {
-  data.config.EXCEPTION_ROBOT_TOKEN = EXCEPTION_ROBOT_TOKEN;
-  console.info('EXCEPTION_ROBOT_TOKEN updated to', EXCEPTION_ROBOT_TOKEN);
+  data.config.EXCEPTION_REPORTER_TOKEN = EXCEPTION_REPORTER_TOKEN;
+  console.info('EXCEPTION_REPORTER_TOKEN updated to', EXCEPTION_REPORTER_TOKEN);
   return data;
 });
