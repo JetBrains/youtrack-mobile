a73a849a491959b08eb9681dfbe8b422fe8b6cf9
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-07-19 15:55:58 +0300

    Fix oAUTH if HUB url has tailing slash

--- src/components/auth/auth__oauth.android.js
+++ src/components/auth/auth__oauth.android.js
@@ -1,16 +1,18 @@
 import {Linking} from 'react-native';
+import urlJoin from 'url-join';
 import qs from 'qs';
 import type {AppConfig} from '../../flow/AppConfig';
 
 function openAuthPage(config: AppConfig) {
-  Linking.openURL([
-    `${config.auth.serverUri}/api/rest/oauth2/auth`,
+  Linking.openURL(encodeURI(urlJoin(
+    config.auth.serverUri,
+    '/api/rest/oauth2/auth',
     '?response_type=code',
     '&access_type=offline',
     `&client_id=${config.auth.clientId}`,
     `&scope=${config.auth.scopes}`,
     `&redirect_uri=${config.auth.landingUrl}`
-  ].join(''));
+  )));
 }
 
 function authorizeInHub(config: AppConfig) {
--- src/components/auth/auth__oauth.ios.js
+++ src/components/auth/auth__oauth.ios.js
@@ -1,19 +1,21 @@
 /* @flow */
 
 import {Linking} from 'react-native';
+import urlJoin from 'url-join';
 import SafariView from '../safari-view/safari-view';
 import qs from 'qs';
 import type {AppConfig} from '../../flow/AppConfig';
 
 async function openAuthPage(config: AppConfig) {
-  const url =  encodeURI([
-    `${config.auth.serverUri}/api/rest/oauth2/auth`,
+  const url = encodeURI(urlJoin(
+    config.auth.serverUri,
+    '/api/rest/oauth2/auth',
     '?response_type=code',
     '&access_type=offline',
     `&client_id=${config.auth.clientId}`,
     `&scope=${config.auth.scopes}`,
     `&redirect_uri=${config.auth.landingUrl}`
-  ].join(''));
+  ));
 
   try {
     await SafariView.isAvailable();
