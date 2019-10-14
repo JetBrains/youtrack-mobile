0cfc5ed848b8977f592376bf4f96c8ea7329d713
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2019-10-14 15:30:03 +0200

    chore(package.json): improve update version script

--- tools/set-ios-version.js
+++ tools/set-ios-version.js
@@ -1,14 +1,18 @@
 const exec = require('child_process').exec;
 
-const BUILD_NUMBER = process.env.buildNumber || 2222.2;
-const VERSION_NUMBER = process.env.versionNumber || 7777;
+const BUILD_NUMBER = process.env.buildNumber || 0.1;
+const VERSION_NUMBER = process.env.versionNumber || 1;
 
 if (!BUILD_NUMBER || !VERSION_NUMBER) {
   console.error('process.env', process.env); //eslint-disable-line
   throw new Error('`versionNumber` and `buildNumber` are required.');
 }
 
-console.log(`Setting version and build number in Xcode project: Marketing = ${VERSION_NUMBER}, build number = ${BUILD_NUMBER}`);//eslint-disable-line
+console.log( //eslint-disable-line
+  (BUILD_NUMBER < 1 ? '\x1b[31m' : '\x1b[32m'),
+  `Update version and build number in Xcode project: Marketing = ${VERSION_NUMBER}, build number = ${BUILD_NUMBER}`,
+  '\x1b[0m'
+);
 
 exec(
   `cd ios && agvtool new-marketing-version ${VERSION_NUMBER} && agvtool new-version -all ${BUILD_NUMBER}`,
