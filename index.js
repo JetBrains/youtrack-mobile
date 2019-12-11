67aa768a81f938369351d65995bae3fe8fc0c90b
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2019-12-11 16:32:46 +0100

    chore: better service naming

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// create Bugsnag reporter as soon as possible
-import Bugsnag from './src/components/report/bugsnag';
+// create exception reporter as soon as possible
+import ExceptioReporter from './src/components/exception-report/exception-reporter';
 
 import YouTrackMobileApp from './src/app';
 import React from 'react-native';
