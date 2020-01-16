de28e5c5c2192ee77d14c447bb60dc76224c1eb7
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-01-16 11:56:13 +0100

    unlink Bugsnag

--- android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainApplication.java
+++ android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.bugsnag.BugsnagReactNative;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -34,7 +33,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            BugsnagReactNative.getPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
