78f0f5a20a9c90bc5e857716dd30c48d5d7b69c0
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-02-27 15:11:08 +0100

    e2e: added jest streamline reporter

--- e2e/init.js
+++ e2e/init.js
@@ -1,14 +1,29 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
 
+const adapter = require('detox/runners/jest/adapter');
+const streamlineReporter = require('detox/runners/jest/streamlineReporter');
+
+jasmine.getEnv().addReporter(adapter);
+jasmine.getEnv().addReporter(streamlineReporter);
+
+
 const JEST_DEFAULT_TIMEOUT = process.env.jetsTimeout || 120000;
 jest.setTimeout(JEST_DEFAULT_TIMEOUT);
-console.log('Jest default timeout is', JEST_DEFAULT_TIMEOUT); //eslint-disable-line
 
 beforeAll(async () => {
   await detox.init(config);
 });
 
 afterAll(async () => {
+  await adapter.afterAll();
   await detox.cleanup();
+});
+
+beforeEach(async () => {
+  await adapter.beforeEach();
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason); //eslint-disable-line
 });
