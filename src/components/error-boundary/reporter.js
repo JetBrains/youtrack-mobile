f4b9315a5b78152faf5e03ee55a2a9ef9ab1c84d
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-04-07 12:37:57 +0200

    YTM-1251 reporting

--- src/components/error-boundary/reporter.js
+++ src/components/error-boundary/reporter.js
@@ -1,6 +1,8 @@
 /* @flow */
 import appPackage from '../../../package.json'; // eslint-disable-line import/extensions
+import DeviceInfo from 'react-native-device-info';
 import log from '../log/log';
+import {getLogs} from '../debug-view/debug-view';
 import {ResourceTypes, getShortEntityType} from '../api/api__resource-types';
 
 const {EXCEPTION_REPORTER_TOKEN} = appPackage.config;
@@ -13,6 +15,8 @@ const SERVER_URI = 'https://youtrack.jetbrains.com/api/issues?fields=idReadable'
 
 const YOUTRACK_MOBILE_PROJECT_ID = '22-174';
 const YOUTRACK_MOBILE_TEAM_ID = '10-603';
+
+export type ErrorData = { summary: string, description: string };
 
 export async function reportCrash(summary: string, description: string): Promise<string> {
   const response = await fetch(SERVER_URI, {
@@ -27,16 +31,43 @@ export async function reportCrash(summary: string, description: string): Promise
       description,
       project: {id: YOUTRACK_MOBILE_PROJECT_ID},
       visibility: {
-        $type: getShortEntityType( ResourceTypes.VISIBILITY_LIMITED),
+        $type: getShortEntityType(ResourceTypes.VISIBILITY_LIMITED),
         permittedGroups: [{id: YOUTRACK_MOBILE_TEAM_ID}]
       }
     })
   });
-  const res: {idReadable: string} = await response.json();
+  const res: { idReadable: string } = await response.json();
 
   if (response.status > 400) {
     throw res;
   }
 
   return res.idReadable;
+}
+
+export async function createErrorData(error: Error): Promise<ErrorData> {
+  const logs = await getLogs();
+  const message = error.toString();
+  const summary = message.split('\n')[0];
+  const description = `
+  App version: ${DeviceInfo.getVersion()};
+  App build: ${DeviceInfo.getBuildNumber()};
+  OS: ${DeviceInfo.getSystemName()};
+  System version: ${DeviceInfo.getSystemVersion()};
+  Device: ${DeviceInfo.getBrand()} ${DeviceInfo.getDeviceId()};
+
+  ============== ERROR ==============
+  \`\`\`
+      ${message}
+  \`\`\`
+
+  ============== LOGS ===============
+  \`\`\`
+  ${logs}
+  \`\`\`
+      `;
+  return {
+    summary,
+    description
+  };
 }
