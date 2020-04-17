ebe326e13abaadc60f6cc15c3f25dfb577144d35
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-04-17 17:45:32 +0200

    YTM-1290: updated <Accounts/> component

--- src/components/account/accounts.js
+++ src/components/account/accounts.js
@@ -1,20 +1,22 @@
 /* @flow */
 
-import {View, Text, TouchableWithoutFeedback, TouchableOpacity, Image, Alert} from 'react-native';
+import {View, Text, TouchableWithoutFeedback, TouchableOpacity, Alert} from 'react-native';
 import React, {PureComponent} from 'react';
 
 import type {StorageState} from '../storage/storage';
 import type {AppConfigFilled} from '../../flow/AppConfig';
 
-import {COLOR_DARK_BORDER} from '../variables/variables';
+import {COLOR_PINK, COLOR_PINK_TRANSPARENT} from '../variables/variables';
 
 import Swiper from 'react-native-swiper';
 import Avatar from '../avatar/avatar';
-import {logOut as logOutIcon, add as addIcon} from '../icon/icon';
+import {IconPlus, IconLogout} from '../icon/icon';
 import {formatYouTrackURL} from '../config/config';
 import clicksToShowCounter from '../debug-view/clicks-to-show-counter';
 import {getStorageState} from '../storage/storage';
+import {HIT_SLOP} from '../common-styles/button';
 
+import avatarStyles from '../avatar/default-avatar.styles';
 import styles from './accounts.styles';
 
 
@@ -66,6 +68,37 @@ export default class Accounts extends PureComponent<Props, void> {
     this.props.onChangeAccount(account);
   };
 
+  renderAccount(account: StorageState, key: string) {
+    const config: AppConfigFilled = account.config;
+    const user = account.currentUser;
+
+    if (!user) {
+      throw new Error(`Account of ${config.backendUrl} has no currentUser`);
+    }
+
+    const avatarUrl = user?.profile?.avatar?.url || '';
+
+    return (
+      <View
+        testID="accountsAccount"
+        key={key}
+        style={styles.accountProfile}
+      >
+        <TouchableWithoutFeedback>
+          <Avatar
+            size={80}
+            userName={user.name}
+            source={{uri: avatarUrl}}
+            style={avatarStyles.size80}
+          />
+        </TouchableWithoutFeedback>
+
+        <Text style={styles.accountProfileName}>{user.name}</Text>
+
+        <Text style={styles.accountProfileServerURL} numberOfLines={1}>{formatYouTrackURL(config.backendUrl)}</Text>
+      </View>
+    );
+  }
 
   render() {
     const {openDebugView, onAddAccount, otherAccounts, isChangingAccount} = this.props;
@@ -77,55 +110,42 @@ export default class Accounts extends PureComponent<Props, void> {
     return (
       <View
         style={styles.accountContainer}
-        testID="accounts">
+        testID="accounts"
+      >
+
         <TouchableOpacity
           testID="accountsAddAccount"
-          style={styles.action}
+          hitSlop={HIT_SLOP}
+          style={styles.accountAction}
           disabled={isChangingAccount}
           onPress={onAddAccount}>
-          <Image style={styles.actionIcon} source={addIcon}/>
+          <IconPlus size={24} color={COLOR_PINK}/>
         </TouchableOpacity>
 
         <Swiper
-          dotColor={COLOR_DARK_BORDER}
-          activeDotColor="white"
+          dotColor={COLOR_PINK_TRANSPARENT}
+          activeDotColor={COLOR_PINK}
           loop={false}
-          height={158}
           scrollEnabled={!isChangingAccount}
           index={accounts.indexOf(storageState)}
           onIndexChanged={(index: number) => this._onChangeAccount(accounts[index])}
           onTouchStart={() => clicksToShowCounter(openDebugView, 'open debug view')}
         >
-          {accounts.map((account, index) => {
-            const config: AppConfigFilled = account.config;
-            const user = account.currentUser;
-            if (!user) {
-              throw new Error(`Account of ${config.backendUrl} has no currentUser`);
-            }
-            const avatarUrl = user.profile && user.profile.avatar && user.profile.avatar.url || '';
-
-            return (
-              <View
-                testID="accountsAccount"
-                key={`${index}_${account.creationTimestamp || 0}`}
-                style={[styles.profileContainer, accounts.length > 1 && styles.profileContainerMultipleAccount]}>
-                <TouchableWithoutFeedback>
-                  <Avatar size={64} userName={user.name} source={{uri: avatarUrl}}/>
-                </TouchableWithoutFeedback>
-
-                <Text style={styles.serverURL} numberOfLines={1}>{formatYouTrackURL(config.backendUrl)}</Text>
-                <Text style={styles.profileName}>{user.name}</Text>
-              </View>
-            );
-          })}
+          {accounts.map(
+            (account, index) => this.renderAccount(
+              account,
+              `${index}_${account.creationTimestamp || 0}`
+            )
+          )}
         </Swiper>
 
         <TouchableOpacity
           testID="accountsOnLogOut"
-          style={styles.action}
+          hitSlop={HIT_SLOP}
+          style={styles.accountAction}
           disabled={isChangingAccount}
           onPress={this._logOut}>
-          <Image style={styles.actionIcon} source={logOutIcon}/>
+          <IconLogout size={22} color={COLOR_PINK}/>
         </TouchableOpacity>
 
       </View>
--- src/components/account/accounts.styles.js
+++ src/components/account/accounts.styles.js
@@ -1,38 +1,37 @@
 import {StyleSheet} from 'react-native';
-import {UNIT, COLOR_GRAY, COLOR_FONT_ON_BLACK} from '../variables/variables';
+import {UNIT, COLOR_FONT} from '../variables/variables';
+import {mainText, secondaryText} from '../common-styles/issue';
 
-const iconHeight = UNIT * 3;
 
 export default StyleSheet.create({
   accountContainer: {
     flexDirection: 'row',
+    marginTop: UNIT * 3,
+    height: 192
   },
-  profileContainer: {
-    flex: 1,
+  accountProfile: {
+    height: 150,
     alignItems: 'center',
-    paddingBottom: UNIT * 2
-  },
-  profileContainerMultipleAccount: {
-    paddingBottom: UNIT * 4
-  },
-  profileName: {
-    color: COLOR_GRAY,
-    marginTop: UNIT/2,
-    marginBottom: UNIT * 2
-  },
-  serverURL: {
-    marginTop: UNIT,
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: COLOR_FONT_ON_BLACK,
-  },
-  actionIcon: {
-    height: iconHeight,
-    resizeMode: 'contain'
-  },
-  action: {
-    flex: 0,
     justifyContent: 'flex-start',
-    height: iconHeight + UNIT
+    marginLeft: UNIT * 3,
+    marginRight: UNIT * 3
+  },
+  accountProfileAvatar: {
+    borderRadius: UNIT
+  },
+  accountProfileName: {
+    ...mainText,
+    marginTop: UNIT * 2,
+    marginBottom: UNIT,
+    fontSize: 18,
+    fontWeight: '500',
+    color: COLOR_FONT,
+  },
+  accountProfileServerURL: {
+    ...secondaryText,
+    color: COLOR_FONT,
+  },
+  accountAction: {
+    height: UNIT * 6
   }
 });
