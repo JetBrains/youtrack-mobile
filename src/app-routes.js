f3d13bcb537c84a55a37c4d6e252698df6879905
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-04-17 12:28:44 +0200

    chore: separated app routes map and app root routes

---
+++ src/app-routes.js
@@ -0,0 +1,17 @@
+/* @flow */
+
+export const routeMap = {
+  AgileBoard: 'AgileBoard',
+  AttachmentPreview: 'AttachmentPreview',
+  CreateIssue: 'CreateIssue',
+  EnterServer: 'EnterServer',
+  Inbox: 'Inbox',
+  IssueList: 'IssueList',
+  LogIn: 'LogIn',
+  Settings: 'Settings',
+  ShowImage: 'ShowImage',
+  SingleIssue: 'SingleIssue',
+  WikiPage: 'WikiPage'
+};
+
+export const rootRoutesList = [routeMap.AgileBoard, routeMap.Inbox, routeMap.IssueList];
