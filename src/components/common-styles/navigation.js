6c87a7afa6fad33e47ffb42f124e8d0833f8e67a
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-04-17 17:47:41 +0200

    YTM-1290: added navigation common styles

---
+++ src/components/common-styles/navigation.js
@@ -0,0 +1,11 @@
+import {COLOR_BLACK, UNIT} from '../../components/variables/variables';
+
+export const menuHeight = UNIT * 8;
+
+export const headerTitle = {
+  color: COLOR_BLACK,
+  fontSize: 20,
+  fontWeight: '500',
+  letterSpacing: 0.13,
+};
+
