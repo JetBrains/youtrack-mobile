280d3de774c5418e788d22330019d95b81261492
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-05-11 12:31:20 +0200

    chore: added platform check utilities

--- src/util/util.js
+++ src/util/util.js
@@ -1,7 +1,16 @@
 /* @flow */
 
 import React from 'react';
+import {Platform} from 'react-native';
 
 export const isReactElement = (element: any) => {
   return React.isValidElement(element);
+};
+
+export const isIOS = () => {
+  return Platform.OS === 'ios';
+};
+
+export const isAndroid = () => {
+  return Platform.OS === 'android';
 };
