cc048d6a724990819f0eecc4faf19ed23ed8e44f
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-05-13 12:48:27 +0200

    parse query string safely #YTM-1313 fixed
    (cherry picked from commit 5a480bfcd6c65c76e6056e09708e2d11d5a08a26)

--- src/components/auth/auth__oauth.android.js
+++ src/components/auth/auth__oauth.android.js
@@ -1,6 +1,8 @@
 import {Linking} from 'react-native';
 import urlJoin from 'url-join';
-import qs from 'qs';
+
+import {parseQueryString} from './auth-helper';
+
 import type {AppConfig} from '../../flow/AppConfig';
 
 function openAuthPage(config: AppConfig) {
@@ -22,8 +24,7 @@ function authorizeInHub(config: AppConfig) {
       Linking.removeEventListener('url', onOpenWithUrl);
       const url = event.url || event;
 
-      const [, query_string] = url.match(/\?(.*)/);
-      const code = qs.parse(query_string).code;
+      const code = parseQueryString(url).code;
       resolve(code);
       Linking.removeEventListener('url', onOpenWithUrl);
     }
