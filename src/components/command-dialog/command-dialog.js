d96d5409f340bf4189d9ee20a952ea590955b7a9
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-05-14 14:58:47 +0200

    Implement Command Dialog #YTM-1319 fixed

--- src/components/command-dialog/command-dialog.js
+++ src/components/command-dialog/command-dialog.js
@@ -2,13 +2,14 @@
 import {View, TouchableOpacity, Text, TextInput, FlatList, ActivityIndicator} from 'react-native';
 import React, {Component} from 'react';
 import styles from './command-dialog.styles';
-import {COLOR_PLACEHOLDER} from '../../components/variables/variables';
+import {COLOR_ICON_MEDIUM_GREY, COLOR_PINK, COLOR_PLACEHOLDER} from '../variables/variables';
 import throttle from 'lodash.throttle';
 import Header from '../../components/header/header';
 import ApiHelper from '../../components/api/api__helper';
 import type {CommandSuggestionResponse, CommandSuggestion, SuggestedCommand} from '../../flow/Issue';
 import ModalView from '../modal-view/modal-view';
 import KeyboardSpacerIOS from '../platform/keyboard-spacer.ios';
+import {IconCheck, IconClose} from '../icon/icon';
 
 const SEARCH_THROTTLE = 30;
 
@@ -33,27 +34,15 @@ type DefaultProps = {
 }
 
 export default class CommandDialog extends Component<Props, State> {
+  static defaultProps: DefaultProps = {
+    onChange: () => {}
+  };
   state: State = {
     displayCancelSearch: false,
     input: '',
     caret: 0
   };
-
-  lastUsedParams: {command: ?string, caret: number} = {command: null, caret: 0};
-
-  static defaultProps: DefaultProps = {
-    onChange: () => {}
-  };
-
-  componentDidMount() {
-    const {initialCommand} = this.props;
-    if (initialCommand) {
-      this.setState({input: initialCommand, caret: initialCommand.length});
-    }
-
-    this.onSearch(initialCommand, initialCommand.length);
-  }
-
+  lastUsedParams: { command: ?string, caret: number } = {command: null, caret: 0};
   onSearch = throttle((command: string, caret: number) => {
     if (this.lastUsedParams.command === command && this.lastUsedParams.caret === caret) {
       return;
@@ -63,6 +52,15 @@ export default class CommandDialog extends Component<Props, State> {
     this.setState({input: command, caret});
     this.props.onChange(command, caret);
   }, SEARCH_THROTTLE);
+
+  componentDidMount() {
+    const {initialCommand} = this.props;
+    if (initialCommand) {
+      this.setState({input: initialCommand, caret: initialCommand.length});
+    }
+
+    this.onSearch(initialCommand, initialCommand.length);
+  }
 
   onApplySuggestion = (suggestion: CommandSuggestion) => {
     const suggestionText = `${suggestion.prefix || ''}${suggestion.option}${suggestion.suffix || ''}`;
@@ -74,7 +72,7 @@ export default class CommandDialog extends Component<Props, State> {
 
   onApply = () => {
     this.props.onApply(this.state.input);
-  }
+  };
 
   _renderInput() {
     const {input} = this.state;
@@ -102,19 +100,10 @@ export default class CommandDialog extends Component<Props, State> {
             canApply && this.onApply();
           }}
           onChangeText={text => this.setState({input: text})}
-          onSelectionChange = {event => this.onSearch(input, event.nativeEvent.selection.start)}
+          onSelectionChange={event => this.onSearch(input, event.nativeEvent.selection.start)}
         />
 
         {isApplying && <ActivityIndicator/>}
-
-        <TouchableOpacity
-          style={styles.applyButton}
-          disabled={!canApply}
-          testID="command-apply"
-          onPress={this.onApply}
-        >
-          <Text style={[styles.applyText, !canApply && styles.applyTextDisabled]}>Apply</Text>
-        </TouchableOpacity>
       </View>
     );
   }
@@ -158,38 +147,48 @@ export default class CommandDialog extends Component<Props, State> {
   _renderSuggestions() {
     const {suggestions} = this.props;
     return (
-      <View style={styles.animatedListContainer}>
-        <View style={styles.listContainer}>
-          {suggestions && (
-            <FlatList
-              contentContainerStyle={styles.suggestionsList}
-              data={suggestions.suggestions}
-              keyExtractor={this._extractKey}
-              renderItem={this._renderSuggestion}
-              keyboardShouldPersistTaps="handled"
-            />
-          )}
-        </View>
+      <View style={styles.listContainer}>
+        {suggestions && (
+          <FlatList
+            contentContainerStyle={styles.suggestionsList}
+            data={suggestions.suggestions}
+            keyExtractor={this._extractKey}
+            renderItem={this._renderSuggestion}
+            keyboardShouldPersistTaps="handled"
+          />
+        )}
       </View>
     );
   }
 
   render() {
+    const {isApplying, suggestions} = this.props;
+    const canApply = !isApplying && suggestions && suggestions.commands.every(command => !command.error);
+
     return (
       <ModalView
         visible
         animationType="fade"
+        style={styles.modal}
       >
         <Header
-          leftButton={<Text>Cancel</Text>}
+          leftButton={<IconClose size={24} color={COLOR_PINK}/>}
+
+          rightButton={
+            <IconCheck testID="command-apply" size={24} color={canApply ? COLOR_PINK : COLOR_ICON_MEDIUM_GREY}/>
+          }
+          onRightButtonClick={() => canApply && this.onApply()}
+
           onBack={this.props.onCancel}
         >
           <Text style={styles.headerText}>{this.props.headerContent}</Text>
         </Header>
+
+        {this._renderInput()}
         {this._renderCommandPreview()}
         {this._renderSuggestions()}
-        {this._renderInput()}
-        <KeyboardSpacerIOS/>
+
+        {<KeyboardSpacerIOS/>}
       </ModalView>
     );
   }
--- src/components/command-dialog/command-dialog.styles.js
+++ src/components/command-dialog/command-dialog.styles.js
@@ -1,94 +1,58 @@
 import {StyleSheet} from 'react-native';
 import {
   UNIT,
-  COLOR_TRANSPARENT_BLACK,
-  COLOR_BLACK,
-  COLOR_SELECTED_DARK,
-  COLOR_PINK,
   COLOR_FONT_ON_BLACK,
-  COLOR_PLACEHOLDER
-} from '../../components/variables/variables';
+  COLOR_BLACK,
+  COLOR_ICON_MEDIUM_GREY, COLOR_MEDIUM_GRAY
+} from '../variables/variables';
+import {formStyles} from '../common-styles/form';
+import {mainText} from '../common-styles/issue';
 
 export default StyleSheet.create({
   modal: {
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  animatedListContainer: {
-    flex: 1,
-    flexGrow: 1,
-    flexShrink: 1,
-    width: '100%',
-    justifyContent: 'flex-end',
-    backgroundColor: COLOR_TRANSPARENT_BLACK
+    backgroundColor: COLOR_FONT_ON_BLACK,
+    paddingLeft: UNIT,
+    paddingRight: UNIT,
   },
   listContainer: {
     flexDirection: 'row',
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
+    paddingLeft: UNIT,
+    paddingRight: UNIT,
   },
   suggestionsList: {
     overflow: 'visible',
     paddingTop: UNIT * 2
   },
-
   headerText: {
     fontSize: 17,
     color: COLOR_FONT_ON_BLACK
   },
-
   inputWrapper: {
-    backgroundColor: COLOR_BLACK,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    padding: UNIT,
   },
   searchInput: {
-    flex: 1,
-    height: UNIT * 4.5,
-    borderRadius: 6,
-    backgroundColor: COLOR_SELECTED_DARK,
-    margin: UNIT,
-    paddingTop: 2,
-    paddingBottom: 2,
-    paddingLeft: UNIT,
-    color: COLOR_PINK,
-    fontSize: 15
-  },
-  keyboardSpacer: {
-    backgroundColor: COLOR_BLACK
-  },
-  applyButton: {
-    paddingRight: UNIT * 2,
-    padding: UNIT
-  },
-  applyText: {
-    fontSize: 16,
-    color: COLOR_PINK
-  },
-  applyTextDisabled: {
-    color: COLOR_PLACEHOLDER
+    ...formStyles.input,
   },
   suggestionRow: {
     flex: 1,
     flexDirection: 'row',
-    padding: UNIT * 2,
     paddingTop: UNIT * 1.5,
-    paddingBottom: UNIT * 1.5,
-    paddingRight: UNIT
+    paddingBottom: UNIT * 2,
+    borderBottomColor: COLOR_MEDIUM_GRAY,
+    borderBottomWidth: 0.5,
   },
   suggestionDescriptionContainer: {
     flex: 1,
     flexGrow: 1,
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginRight: UNIT * 2
+    marginRight: UNIT * 2,
   },
   suggestionDescription: {
-    fontSize: 16,
-    color: COLOR_PLACEHOLDER
+    ...mainText,
+    color: COLOR_ICON_MEDIUM_GREY
   },
   suggestionTextContainer: {
     flex: 1,
@@ -97,18 +61,20 @@ export default StyleSheet.create({
     flexShrink: 0
   },
   suggestionText: {
-    fontSize: 24,
+    ...mainText,
     fontWeight: '300',
-    color: COLOR_FONT_ON_BLACK
+    color: COLOR_BLACK
   },
   commandPreview: {
-    width: '100%',
-    padding: UNIT * 2,
-    paddingTop: UNIT / 2,
-    backgroundColor: COLOR_TRANSPARENT_BLACK
+    paddingTop: UNIT * 2,
+    paddingBottom: UNIT * 2,
+    marginLeft: UNIT,
+    marginRight: UNIT,
+    borderBottomColor: COLOR_MEDIUM_GRAY,
+    borderBottomWidth: 0.5,
   },
   commandDescription: {
-    color: COLOR_FONT_ON_BLACK
+    color: COLOR_BLACK
   },
   commandDescriptionError: {
     color: 'red'
