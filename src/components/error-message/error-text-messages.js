97e1daed05f5a4574a812d9d09de3e3a5e84fcd9
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-07-30 12:46:24 +0200

    fixed typo

--- src/components/error-message/error-text-messages.js
+++ src/components/error-message/error-text-messages.js
@@ -5,7 +5,7 @@ const LOG_IN_2FA_TIP = 'If you have enabled 2-factor authentication, use the "Lo
 const NETWORK_PROBLEM_TIPS = [
   '\nMake sure that your YouTrack installation is accessible over the internet.',
   'If your YouTrack installation is hosted on a web server from a non-standard port (like 80 for HTTP or 443 for HTTPS), include the port number after the URL: youtrack-example.com:PORT',
-  '\nYConnections to servers that use self-signed SSL/TLS certificates are not supported.',
+  '\nConnections to servers that use self-signed SSL/TLS certificates are not supported.',
 ];
 
 const YT_SUPPORTED_VERSION = `YouTrack Mobile requires YouTrack version 2016.2 or later.`;
