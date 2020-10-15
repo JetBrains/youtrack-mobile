0a55e6b8938f0574aad3f0527d7607d1c73c510e
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-10-15 09:55:24 +0200

    Revert "Disable iOS dark mode #YTM-10697 fixed"
    This reverts commit 290a340e

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -2,12 +2,8 @@
 
 set -e -x
 
-echo "Applying patches"
-yarn patch-package
-
 echo "Running JETIFIER"
 npx jetifier
 
 echo "Running IOS-INSTALL-THIRD-PARTY"
 cd node_modules/react-native/React/..; exec ./scripts/ios-install-third-party.sh
-
