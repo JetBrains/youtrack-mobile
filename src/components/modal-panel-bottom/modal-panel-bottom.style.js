f4fa11ca3e87524b6e1f128c3323c104e046d8d6
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2020-11-08 15:17:13 +0100

    Remove background in Modal Bottom Panel #YTM-11890 fixed

--- src/components/modal-panel-bottom/modal-panel-bottom.style.js
+++ src/components/modal-panel-bottom/modal-panel-bottom.style.js
@@ -7,16 +7,15 @@ export default EStyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-end',
-    backgroundColor: '$mask',
     marginTop: -UNIT * 6
   },
   content: {
     padding: UNIT * 2,
     paddingTop: UNIT,
     paddingBottom: UNIT * 3,
-    borderTopLeftRadius: UNIT,
-    borderTopRightRadius: UNIT,
-    backgroundColor: '$background'
+    backgroundColor: '$background',
+    borderColor: '$boxBackground',
+    borderTopWidth: 1.5
   },
   title: {
     fontSize: HEADER_FONT_SIZE,
