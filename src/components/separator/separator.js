e8c993616bae7be9d58b7fd3c288d394afe72497
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-01-20 11:10:31 +0100

    chore(components): introduced <Separator> component

---
+++ src/components/separator/separator.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+import React from 'react';
+
+import {View} from 'react-native';
+
+import separatorStyles from './separator.styles';
+
+import type {ViewStyleProp} from 'react-native/Libraries/StyleSheet/StyleSheet';
+
+type Props = {
+  fitWindow?: boolean,
+  indent?: boolean,
+  styles?: ViewStyleProp,
+};
+
+const Separator = (props: Props) => {
+  const {styles, indent, fitWindow, ...rest} = props;
+  return (
+    <View
+      {...rest}
+      style={[
+        fitWindow
+          ? separatorStyles.rowSeparatorFit
+          : separatorStyles.rowSeparator,
+        indent && separatorStyles.indent,
+        styles
+      ]}
+    />
+  );
+};
+
+export default React.memo<Props>(Separator);
