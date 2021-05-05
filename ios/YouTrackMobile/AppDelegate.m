f1f90b780dfd827b1cab1bf34e0b8acb40c9367b
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-05-05 10:43:40 +0200

    YTM-12969 IOS

--- ios/YouTrackMobile/AppDelegate.m
+++ ios/YouTrackMobile/AppDelegate.m
@@ -97,6 +97,36 @@ ClearKeychainIfNecessary();
 #endif
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application {
+// fill screen with a colour
+UIView *colourView = [[UIView alloc]initWithFrame:self.window.frame];
+  if (@available(iOS 13.0, *)) {
+    colourView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    colourView.backgroundColor = [UIColor whiteColor];
+  }
+  colourView.tag = 1234;
+  colourView.alpha = 0;
+  [self.window addSubview:colourView];
+  [self.window bringSubviewToFront:colourView];
+  // fade in the view
+  [UIView animateWithDuration:0.5 animations:^{
+    colourView.alpha = 1;
+  }];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  // grab a reference to our coloured view
+  UIView *colourView = [self.window viewWithTag:1234];
+  // fade away colour view from main view
+  [UIView animateWithDuration:0.5 animations:^{
+  colourView.alpha = 0;
+  } completion:^(BOOL finished) {
+  // remove when finished fading
+  [colourView removeFromSuperview];
+  }];
+}
+
 // RNNotifications
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
   [RNNotifications didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
