9b5f3f9a2449273d81dddbfa9e36c29343f67b9a
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-06-14 12:09:23 +0200

    SafeAreaView doesn't add proper vertical spaces for iPhone 12 #YTM-13408 fixed

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -4,6 +4,7 @@ set -e -x
 
 echo "Running JETIFIER"
 npx jetifier
+npx patch-package
 
 echo "Running IOS-INSTALL-THIRD-PARTY"
 cd node_modules/react-native/React/..; exec ./scripts/ios-install-third-party.sh
