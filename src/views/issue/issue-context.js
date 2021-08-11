96c251a68ea1e68aa039eede1897aaaf5eb72983
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-08-11 10:47:25 +0200

    chore(Flow): fixed type errors

--- src/views/issue/issue-context.js
+++ src/views/issue/issue-context.js
@@ -4,4 +4,4 @@ import React from 'react';
 
 import type {IssueContextData} from '../../flow/Issue';
 
-export const IssueContext = React.createContext<IssueContextData>(null);
+export const IssueContext: React$Context<IssueContextData> = React.createContext<IssueContextData>(null);
