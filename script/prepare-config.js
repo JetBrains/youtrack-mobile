a8830b80ab62a9dcdd4a4404a77e4567c89381a8
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-09-28 23:24:16 +0200

    YTM-13131 restore fulfilling the config

--- script/prepare-config.js
+++ script/prepare-config.js
@@ -5,11 +5,12 @@ const KONNECTOR_URL = process.env.konnektorURL;
 const BUILD_NUMBER = process.env.buildNumber;
 const VERSION_NUMBER = process.env.versionNumber;
 const EXCEPTION_REPORTER_TOKEN = process.env.exeptionReporterToken;
+const BUGSNAG_TOKEN = process.env.bugsnagToken;
 
 
-if (!ANALYTICS_ID || !KONNECTOR_URL || !BUILD_NUMBER || !VERSION_NUMBER) {
+if (!ANALYTICS_ID || !KONNECTOR_URL || !BUILD_NUMBER || !VERSION_NUMBER || !BUGSNAG_TOKEN) {
   console.error('process.env', process.env); //eslint-disable-line
-  throw new Error('Required ENV params `ANALYTICS_ID`, `KONNECTOR_URL`, `BUILD_NUMBER`, `VERSION_NUMBER` are not set');
+  throw new Error('Required ENV params `ANALYTICS_ID`, `KONNECTOR_URL`, `BUILD_NUMBER`, `VERSION_NUMBER`, `BUGSNAG_TOKEN` are not set');
 }
 
 updateJsonFile('package.json', data => {
@@ -18,6 +19,9 @@ updateJsonFile('package.json', data => {
   if (!process.env.STAGING) {
     data.config.KONNECTOR_URL = KONNECTOR_URL;
   }
+  data.config.BUGSNAG_TOKEN = BUGSNAG_TOKEN;
+  data.bugsnag.version = VERSION_NUMBER;
+  data.bugsnag.token = BUGSNAG_TOKEN;
 
   console.info('Config', data.config); //eslint-disable-line
 
