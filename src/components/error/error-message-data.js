8d74d842ebf9722f151f6cb7bd49cab52015f3d7
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-11-19 14:48:44 +0100

    YTM-15706 remove unused code

--- src/components/error/error-message-data.js
+++ src/components/error/error-message-data.js
@@ -6,9 +6,6 @@ import {DEFAULT_ERROR_MESSAGE} from './error-messages';
 import type {ErrorMessageData} from '../../flow/Error';
 import {HTTP_STATUS} from './error-http-codes';
 
-const useBrowserLoginOptionMessageData: ErrorMessageData = {
-  title: 'Two-factor authentication is enabled. Use "Log in with Browser" option.',
-};
 
 const unauthorizedErrorMessageData: ErrorMessageData = {
   title: 'Woah, you can\'t touch this!',
@@ -46,8 +43,9 @@ export const ERROR_MESSAGE_DATA: { [string]: ErrorMessageData } = {
   USER_BANNED: {
     title: 'User account is banned',
   },
-  '2fa_required': useBrowserLoginOptionMessageData,
-  'hub-auth-2fa-enabled': useBrowserLoginOptionMessageData,
+  '2fa_required': {
+    title: 'Two-factor authentication is enabled. Use "Log in with Browser" option.',
+  },
 
   [HTTP_STATUS.UNAUTHORIZED]: unauthorizedErrorMessageData,
   [HTTP_STATUS.FORBIDDEN]: unauthorizedErrorMessageData,
