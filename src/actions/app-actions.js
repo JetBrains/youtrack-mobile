7f1f8b302a93847f73315913f68f725180fd9137
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-11-25 15:32:39 +0100

    YTM-15844 set info about a device type earlier

--- src/actions/app-actions.js
+++ src/actions/app-actions.js
@@ -426,7 +426,6 @@ export function loadUserPermissions(): Action {
 export function completeInitialization(issueId: string | null = null, navigateToActivity: boolean = false): Action {
   return async (dispatch: (any) => any, getState: () => AppState, getApi: () => Api) => {
     log.debug('Completing initialization');
-    dispatch({type: types.SET_IS_TABLET, isTablet: DeviceInfo.isTablet()});
     await dispatch(loadUser());
     await dispatch(loadUserPermissions());
     await dispatch(cacheProjects());
@@ -590,6 +589,7 @@ function subscribeToURL(): Action {
 
 export function initializeApp(config: AppConfigFilled, issueId: string | null, navigateToActivity: boolean): Action {
   return async (dispatch: (any) => any, getState: () => AppState, getApi: () => Api): any => {
+    dispatch({type: types.SET_IS_TABLET, isTablet: DeviceInfo.isTablet()});
     if (Router._getNavigator()) {
       Router.navigateToDefaultRoute(
         {
