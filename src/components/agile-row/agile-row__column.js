474958d6c19d4b42ab1b966dadb22f46bc71f27a
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2021-12-07 16:57:32 +0100

    YTM-16088 Agile

--- src/components/agile-row/agile-row__column.js
+++ src/components/agile-row/agile-row__column.js
@@ -4,8 +4,9 @@ import type {Node} from 'React';
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 
-import { DropZone } from '../draggable';
+import {DropZone} from '../draggable';
 import {IconAdd} from '../icon/icon';
+import {isSplitView} from '../responsive/responsive-helper';
 
 import styles from './agile-row.styles';
 
@@ -20,11 +21,10 @@ type ColumnProps = {
   renderIssueCard: (issue: IssueFull) => any,
   uiTheme: UITheme,
   zoomedIn?: boolean,
-  isTablet: boolean,
 }
 
 export default function AgileRowColumn(props: ColumnProps): Node {
-  const {cell, uiTheme, zoomedIn, isTablet} = props;
+  const {cell, uiTheme, zoomedIn} = props;
   const issues: Array<IssueFull> = cell.issues || [];
 
   return (
@@ -42,7 +42,7 @@ export default function AgileRowColumn(props: ColumnProps): Node {
         onPress={() => props.onTapCreateIssue(cell.column.id, cell.id)}
         style={[
           styles.addCardButton,
-          isTablet && zoomedIn ? styles.addCardButtonTablet : null,
+          isSplitView() && zoomedIn ? styles.addCardButtonTablet : null,
         ]}
       >
         <IconAdd color={uiTheme.colors.$link} size={18}/>
--- src/views/agile-board/agile-board__sprint.js
+++ src/views/agile-board/agile-board__sprint.js
@@ -20,7 +20,6 @@ type Props = {
   onTapCreateIssue: (columnId: string, cellId: string) => void,
   onCollapseToggle: (row: AgileBoardRow) => void,
   uiTheme: UITheme,
-  isTablet: boolean,
 };
 
 
@@ -67,7 +66,7 @@ export default class AgileBoardSprint extends Component<Props, void> {
   };
 
   renderCard: ((issue: AnyIssue) => Node) = (issue: AnyIssue) => {
-    const {sprint, zoomedIn, canRunCommand, onTapIssue, uiTheme, isTablet} = this.props;
+    const {sprint, zoomedIn, canRunCommand, onTapIssue, uiTheme} = this.props;
     const canDrag = sprint.agile.isUpdatable || canRunCommand(issue);
 
     return (
@@ -82,14 +81,13 @@ export default class AgileBoardSprint extends Component<Props, void> {
           estimationField={sprint.agile.estimationField}
           zoomedIn={zoomedIn}
           uiTheme={uiTheme}
-          isTablet={isTablet}
         />
       </Draggable>
     );
   };
 
   renderOrphan: ((board: Board) => Node) = (board: Board) => {
-    const {zoomedIn, isTablet} = this.props;
+    const {zoomedIn} = this.props;
 
     return (
       <BoardRow
@@ -98,14 +96,13 @@ export default class AgileBoardSprint extends Component<Props, void> {
         row={board.orphanRow}
         zoomedIn={zoomedIn}
         columns={board.columns}
-        isTablet={isTablet}
         {...this.createCommonRowProps()}
       />
     );
   };
 
   render(): null | Node {
-    const {sprint, zoomedIn, isTablet} = this.props;
+    const {sprint, zoomedIn} = this.props;
     const board: Board = sprint?.board;
 
     if (!sprint || !board) {
@@ -128,7 +125,6 @@ export default class AgileBoardSprint extends Component<Props, void> {
               row={swimlane}
               zoomedIn={zoomedIn}
               columns={board.columns}
-              isTablet={isTablet}
               {...this.createCommonRowProps()}
             />
           );
