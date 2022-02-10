8a2e230414e61f6d382d2209008fe1a8369fedf4
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-02-10 14:32:37 +0100

    refactor: added resolve from './src' to get rid of absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: ['.', './src', '../..', '../../src'],
+      extensions: ['.js', '.jsx', '.ts', '.json'],
+      alias: {
+        actions: './src/actions',
+        assets: './src/assets',
+        components: './src/components',
+        flow: './src/flow',
+        util: './src/util',
+        views: './src/views',
+      },
+    }],
+  ],
 };
