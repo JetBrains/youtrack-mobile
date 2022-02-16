bd9e59695df5a9b663daf5c85fb889c265d55eb0
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-02-16 15:14:25 +0100

    YTM-16940 react-syntax-highlighter

--- src/components/wiki/code-renderer.js
+++ src/components/wiki/code-renderer.js
@@ -5,7 +5,7 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import SyntaxHighlighter from 'react-native-syntax-highlighter';
-import { idea, darcula } from 'react-syntax-highlighter/dist/styles';
+import { idea, darcula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import {decodeHTML} from 'entities';
 import Router from '../router/router';
