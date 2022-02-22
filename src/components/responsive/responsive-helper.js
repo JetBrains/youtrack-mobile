67b348390e1cc76f8c70866569e9224ff1e80dcc
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-02-22 23:34:35 +0100

    YTM-16942 improved the code

--- src/components/responsive/responsive-helper.js
+++ src/components/responsive/responsive-helper.js
@@ -5,19 +5,22 @@ import {Dimensions} from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 import {getStorageState} from '../storage/storage';
-import log from '../log/log';
 
 const tabletSplitViewFactor: number = 0.66;
 
 const isSplitView = (): boolean => {
-  const windowWidth: number = Dimensions.get('window').width;
-  const screenWidth: number = Dimensions.get('screen').width;
   let isHandsetModeForced: boolean = false;
   try {
     isHandsetModeForced = !!getStorageState().forceHandsetMode;
-    log.log('Headset mode', isHandsetModeForced);
   } catch (e) {}
-  return DeviceInfo.isTablet() && (windowWidth >= screenWidth * tabletSplitViewFactor) && !isHandsetModeForced;
+
+  if (isHandsetModeForced === true) {
+    return false;
+  }
+
+  const windowWidth: number = Dimensions.get('window').width;
+  const screenWidth: number = Dimensions.get('screen').width;
+  return DeviceInfo.isTablet() && (windowWidth >= screenWidth * tabletSplitViewFactor);
 };
 
 export {
