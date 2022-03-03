d6845faebb93fb1c00332aa54e3520f8fa1067db
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-03-03 16:06:03 +0100

    YTM-17111

--- src/components/i18n/i18n.js
+++ src/components/i18n/i18n.js
@@ -2,12 +2,13 @@
 
 import {gt} from './i18n-gettext';
 
+type Params = { [p: string]: string | number | undefined } | undefined;
 
-export function i18n(text: string): string {
-  return gt.gettext(text);
+export function i18n(text: string = '', params?: Params): string {
+  return gt.gettext(createNgettextMessage(text, params));
 }
 
-export function i18nPlural(count: number, text: string, textPlural: string, params): string {
+export function i18nPlural(count: number, text: string, textPlural: string, params?: Params): string {
   return gt.ngettext(
     createNgettextMessage(text, params),
     createNgettextMessage(textPlural, params),
@@ -16,7 +17,7 @@ export function i18nPlural(count: number, text: string, textPlural: string, para
 }
 
 
-function createNgettextMessage(message: string, params?: { [p: string]: string | number | undefined } | undefined) {
+function createNgettextMessage(message: string, params?: Params) {
   let msg: string = message.slice(0);
   if (params) {
     message.replace(/{{([^}]+)}}/g, (pattern: string, key: string) => {
