4c8167a9463737e803e6c69a425eb7cb3ccd4e52
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-03-10 13:40:52 +0100

    YTM-17110 added unit tests

---
+++ src/components/i18n/i18n-gettext.spec.js
@@ -0,0 +1,9 @@
+import gt from './i18n-gettext';
+
+describe('i18n', () => {
+  it('should get node-gettext singleton', () => {
+    expect(gt).toBeDefined();
+    expect(typeof gt.gettext).toEqual('function');
+    expect(typeof gt.ngettext).toEqual('function');
+  });
+});
