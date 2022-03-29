118789eb87a6e2425988d93a6b4dac93950c6f0d
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-03-29 16:27:19 +0200

    YTM-17324 fixed running xcodebuild; Android command goes first

--- script/bugsnag.sh
+++ script/bugsnag.sh
@@ -3,22 +3,6 @@
 set -ex
 
 
-# iOS
-
-echo "IOS: Creating source maps..."
-export CONFIGURATION_BUILD_DIR="$(xcodebuild -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -showBuildSettings | grep CONFIGURATION_BUILD_DIR)"
-echo "IOS: Source map path is $(CONFIGURATION_BUILD_DIR)"
-
-echo "IOS: Source maps uploading..."
-bugsnag-source-maps upload-react-native \
-  --api-key "$1" \
-  --app-version "$3" \
-  --platform ios \
-  --source-map "$TMPDIR"/"$(md5 -qs "$CONFIGURATION_BUILD_DIR")"-main.jsbundle.map \
-  --bundle "$CONFIGURATION_BUILD_DIR"/main.jsbundle
-echo "IOS: Source maps uploaded"
-
-
 # Android
 
 echo "ANDROID: Generated source maps"
@@ -32,3 +16,20 @@ bugsnag-source-maps upload-react-native \
   --source-map android/app/build/generated/sourcemaps/react/release/index.android.bundle.map \
   --bundle android/app/build/generated/assets/react/release/index.android.bundle
 echo "ANDROID: Source maps uploaded"
+
+
+# iOS
+
+echo "IOS: Creating source maps..."
+xcdbuild=$(which xcodebuild)
+export CONFIGURATION_BUILD_DIR="$($xcdbuild -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -showBuildSettings | grep CONFIGURATION_BUILD_DIR)"
+echo "IOS: Source map path is $(CONFIGURATION_BUILD_DIR)"
+
+echo "IOS: Source maps uploading..."
+bugsnag-source-maps upload-react-native \
+  --api-key "$1" \
+  --app-version "$3" \
+  --platform ios \
+  --source-map "$TMPDIR"/"$(md5 -qs "$CONFIGURATION_BUILD_DIR")"-main.jsbundle.map \
+  --bundle "$CONFIGURATION_BUILD_DIR"/main.jsbundle
+echo "IOS: Source maps uploaded"
