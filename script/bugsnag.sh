88ea89c3594179e49c2944d777adbaef68a6128b
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-03-29 13:26:52 +0200

    Generate source maps properly #YTM-17324 fixed

--- script/bugsnag.sh
+++ script/bugsnag.sh
@@ -2,50 +2,33 @@
 
 set -ex
 
-echo "Cleaning"
-rm -rf $2
-mkdir $2
 
 # iOS
+
 echo "IOS: Creating source maps..."
-react-native bundle \
-  --dev false \
-  --entry-file index.js \
-  --platform ios \
-  --sourcemap-output $2/ios-release.bundle.map \
-  --bundle-output $2/ios-release.bundle
+export CONFIGURATION_BUILD_DIR="$(xcodebuild -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -showBuildSettings | grep CONFIGURATION_BUILD_DIR)"
+echo "IOS: Source map path is $(CONFIGURATION_BUILD_DIR)"
 
-while [ ! -f $2/ios-release.bundle.map ]; do sleep 1; done
-while [ ! -f $2/ios-release.bundle ]; do sleep 1; done
-
-echo "IOS: Source maps created, uploading..."
+echo "IOS: Source maps uploading..."
 bugsnag-source-maps upload-react-native \
-  --api-key $1 \
-  --app-version $3 \
+  --api-key "$1" \
+  --app-version "$3" \
   --platform ios \
-  --source-map $2/ios-release.bundle.map \
-  --bundle $2/ios-release.bundle
+  --source-map "$TMPDIR"/"$(md5 -qs "$CONFIGURATION_BUILD_DIR")"-main.jsbundle.map \
+  --bundle "$CONFIGURATION_BUILD_DIR"/main.jsbundle
 echo "IOS: Source maps uploaded"
 
 
 # Android
 
-echo "ANDROID: Creating source maps..."
-react-native bundle \
-  --dev false \
-  --entry-file index.js \
-  --platform android \
-  --sourcemap-output $2/android-release.bundle.map \
-  --bundle-output $2/android-release.bundle
+echo "ANDROID: Generated source maps"
+ls android/app/build/generated/sourcemaps/react/release
 
-while [ ! -f $2/android-release.bundle.map ]; do sleep 1; done
-while [ ! -f $2/android-release.bundle ]; do sleep 1; done
-
-echo "ANDROID: Source maps created, uploading..."
+echo "ANDROID: Source maps uploading..."
 bugsnag-source-maps upload-react-native \
-  --api-key $1 \
-  --app-version $3 \
+  --api-key "$1" \
+  --app-version "$3" \
   --platform android \
-  --source-map $2/android-release.bundle.map \
-  --bundle $2/android-release.bundle
+  --source-map android/app/build/generated/sourcemaps/react/release/index.android.bundle.map \
+  --bundle android/app/build/generated/assets/react/release/index.android.bundle
 echo "ANDROID: Source maps uploaded"
