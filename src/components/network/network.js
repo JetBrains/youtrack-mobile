6f9873547b1262f1d21908f35f0e9b516cbd0fb4
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-04-07 13:54:10 +0200

    YTM-17332 fetch network state after switching in a foreground

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -1,47 +1,63 @@
 /* @flow */
 
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
+import {AppState} from 'react-native';
 
 import NetInfo from '@react-native-community/netinfo';
 import Toast, {DURATION} from 'react-native-easy-toast';
+import {isIOSPlatform} from '../../util/util';
 import {setNetworkState} from '../../actions/app-actions';
 import {useDispatch, useSelector} from 'react-redux';
 
 import styles from './network.styles';
 
-import type {AppState} from '../../reducers';
 import type {NetInfoState} from '@react-native-community/netinfo';
 import type {Node} from 'React';
+import type {AppStateValues} from 'react-native/Libraries/AppState/AppState';
 
+export function useIsNetworkConnected() {
+  const networkState: ?NetInfoState = useSelector((appState: AppState) => appState?.app?.networkState);
+  return networkState?.isConnected;
+}
 
 export default function NetworkPopup(): Node {
+  const toastInstance = useRef(null);
   const dispatch: Function = useDispatch();
-  const networkState: ?NetInfoState = useSelector((appState: AppState) => appState?.app?.networkState);
-  let toastInstance;
+  const isConnected: ?boolean = useIsNetworkConnected();
 
-  const updateNetworkState = (state: NetInfoState) => {
-    if (state.isConnected !== networkState?.isConnected) {
+  const updateNetworkState = useCallback((state: NetInfoState) => {
+    if (state.isConnected !== isConnected) {
       // eslint-disable-next-line no-unused-vars
       const {details, ...rest} = state;
       dispatch(setNetworkState(rest));
-      if (toastInstance) {
-        if (rest.isConnected === false) {
-          toastInstance.show('Offline', DURATION.FOREVER);
-        } else if (rest.isConnected === true) {
-          toastInstance.close();
+      if (toastInstance.current) {
+        if (rest?.isConnected === false) {
+          toastInstance.current.show('Offline', DURATION.FOREVER);
+        } else if (rest?.isConnected === true) {
+          toastInstance.current?.close();
         }
       }
     }
-  };
+  }, [dispatch, isConnected]);
 
   useEffect(() => {
-    NetInfo.fetch().then(updateNetworkState);
-    return NetInfo.addEventListener(updateNetworkState);
+    const unsubscribeNetInfoListener = NetInfo.addEventListener(updateNetworkState);
+    const appStateListener = AppState.addEventListener('change', (nextAppState: AppStateValues) => {
+      if (isIOSPlatform() && nextAppState === 'active') {
+        NetInfo.fetch().then(updateNetworkState);
+      }
+    });
+    return () => {
+      unsubscribeNetInfoListener();
+      if (appStateListener) {
+        appStateListener.remove();
+      }
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  },[]);
 
   return <Toast
-    ref={(instance) => instance && (toastInstance = instance)}
+    ref={toastInstance}
     style={styles.container}
     position="top"
     positionValue={40}
