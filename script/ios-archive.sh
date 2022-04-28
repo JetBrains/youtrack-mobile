f2cdc17c0c4504a8513ce5b6f030783fedbc94d0
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-04-28 22:52:22 +0200

    YTM-17324 move ios archive script back into package.json

--- script/ios-archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-xcodebuild archive -allowProvisioningUpdates -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -archivePath ios/build/YouTrackMobile[Release].xcarchive CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED='NO'
