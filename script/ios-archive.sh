45106b724a07dc396d960a771a1cc5938d115d43
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-04-28 22:11:12 +0200

    YTM-17324 use export in package.json

--- script/ios-archive.sh
+++ script/ios-archive.sh
@@ -2,5 +2,4 @@
 
 set -ex
 
-export RCT_NO_LAUNCH_PACKAGER=true
 xcodebuild archive -allowProvisioningUpdates -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -archivePath ios/build/YouTrackMobile[Release].xcarchive CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED='NO'
