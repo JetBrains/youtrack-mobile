af6c0804ce0f081a083766c81e9e9145b50e6dd6
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-04-28 07:52:20 +0200

    YTM-17324 fix bash command

--- script/ios-archive.sh
+++ script/ios-archive.sh
@@ -2,14 +2,12 @@
 
 set -ex
 
-xcdbuild=$(which xcodebuild)
-
 echo "IOS: Creating archive and source maps files..."
-xcdbuild archive -allowProvisioningUpdates -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -archivePath ios/build/YouTrackMobile[Release].xcarchive
+xcodebuild archive -allowProvisioningUpdates -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -archivePath ios/build/YouTrackMobile[Release].xcarchive
 
 
 echo "IOS: create JS bundle and source maps files..."
-CONFIGURATION_BUILD_DIR="$($xcdbuild -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -showBuildSettings | grep CONFIGURATION_BUILD_DIR)"
+CONFIGURATION_BUILD_DIR="$(xcodebuild -workspace ios/YouTrackMobile.xcworkspace -scheme YouTrackMobile[Release] -showBuildSettings | grep CONFIGURATION_BUILD_DIR)"
 export CONFIGURATION_BUILD_DIR
 echo "IOS: Source map path dir: $(CONFIGURATION_BUILD_DIR)"
 
