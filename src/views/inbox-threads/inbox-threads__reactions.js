24b945050ce97eddd2dd2dc36b974ae533b729a1
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-07-18 23:23:23 +0200

    YTM-18068 reaction

--- src/views/inbox-threads/inbox-threads__reactions.js
+++ src/views/inbox-threads/inbox-threads__reactions.js
@@ -3,13 +3,12 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import CommentReactions from 'components/comment/comment-reactions';
 import ReactionIcon from 'components/reactions/reaction-icon';
 import StreamComment from 'components/activity-stream/activity__stream-comment';
+import ThreadCommentReactions from './inbox-threads__item-comment-reactions';
 import ThreadItem from './inbox-threads__item';
 import {i18n} from 'components/i18n/i18n';
 
-import styles from './inbox-threads.styles';
 import stylesInbox from '../inbox/inbox.styles';
 
 import type {Activity} from 'flow/Activity';
@@ -24,7 +23,7 @@ interface Props {
   thread: InboxThread;
 }
 
-export default function InboxThreadReaction({thread, currentUser, onPress}: Props) {
+const InboxThreadReaction = ({thread, currentUser, onPress}: Props) => {
   const activity: Activity = thread.messages[0].activities[0];
   const reaction: Reaction = activity.added[0] || activity.removed[0];
   const comment: ?IssueComment = activity?.comment;
@@ -48,11 +47,7 @@ export default function InboxThreadReaction({thread, currentUser, onPress}: Prop
               added: [comment],
             }}
           />
-          <CommentReactions
-            style={styles.threadCommentReactions}
-            comment={comment}
-            currentUser={currentUser}
-          />
+          <ThreadCommentReactions activity={activity} currentUser={currentUser}/>
         </>
       )}
       onPress={onPress}
@@ -62,4 +57,8 @@ export default function InboxThreadReaction({thread, currentUser, onPress}: Prop
       timestamp={thread.notified}
     />
   );
-}
+};
+
+export default React.memo(InboxThreadReaction, (prev: Props, next: Props) => {
+  return prev.thread === next.thread;
+});
