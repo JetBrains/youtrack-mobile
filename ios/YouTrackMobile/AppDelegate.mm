576686c81cda951e734a8bd531cd01fc3652ffa7
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-11-17 15:53:26 +0100

    YTM-19096 removed Flipper obsolete code from AppDelegate

--- ios/YouTrackMobile/AppDelegate.mm
+++ ios/YouTrackMobile/AppDelegate.mm
@@ -9,18 +9,6 @@
 
 #import "RNNotifications.h"
 
-#if DEBUG
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#endif
-#endif
-
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -72,8 +60,6 @@ static void ClearKeychainIfNecessary() {
 
   ClearKeychainIfNecessary();
 
-  [self initializeFlipper:application];
-
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   
   #if RCT_NEW_ARCH_ENABLED
@@ -115,19 +101,6 @@ static void ClearKeychainIfNecessary() {
   return YES;
 }
 
-- (void) initializeFlipper:(UIApplication *)application {
-  #if DEBUG
-  #ifdef FB_SONARKIT_ENABLED
-    FlipperClient *client = [FlipperClient sharedClient];
-    SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-    [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
-    [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-    [client addPlugin: [FlipperKitReactPlugin new]];
-    [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-    [client start];
-  #endif
-  #endif
-}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
