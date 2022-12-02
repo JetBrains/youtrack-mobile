2630ce0f0b76ac2c87a26b0a679c6a32d384567c
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-12-02 12:57:58 +0100

    YTM-17324 use `app-bundle-version` for iOS

--- script/bugsnag-upload-ios.sh
+++ script/bugsnag-upload-ios.sh
@@ -6,7 +6,7 @@ set -ex
 echo "IOS: Source maps uploading..."
 bugsnag-source-maps upload-react-native \
   --api-key "$1" \
-  --app-version "$2" \
+  --app-bundle-version "$2" \
   --platform ios \
   --source-map dist/main.jsbundle.map \
   --bundle dist/main.jsbundle
