005ff5135ca030f24620d935263c5e6fe0e66fe0
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-12-13 18:45:54 +0100

    YTM-19317

--- src/views/issue/modal/issue.modal-reducers.js
+++ src/views/issue/modal/issue.modal-reducers.js
@@ -2,25 +2,20 @@
 
 import {Slice} from '@reduxjs/toolkit';
 
-import {attachmentTypes} from './issue.modal__attachment-actions-and-types';
+import {attachmentTypes, modalIssueNamespace} from './issue.modal__attachment-actions-and-types';
 import {commandDialogNamespace} from '../issue-action-types';
+import {createAttachmentReducer, createIssueReduxSlice, initialState} from '../issue-base-reducer';
 import {createCommandDialogReducers} from 'components/command-dialog/command-dialog-reducer';
 import {ON_NAVIGATE_BACK} from 'actions/action-types';
 import {routeMap} from '../../../app-routes';
-import {
-  createAttachmentReducer,
-  createIssueReduxSlice,
-  initialState,
-} from '../issue-base-reducer';
+
 import type {IssueState} from '../issue-base-reducer';
 
 export type {IssueState as State}; //TODO
 export {initialState}; //TODO
 
-export const singleIssueNamespace = 'modal';
-
 const {actions, reducer}: typeof Slice = createIssueReduxSlice(
-  singleIssueNamespace,
+  modalIssueNamespace,
   {
     ...createAttachmentReducer(attachmentTypes),
     ...createCommandDialogReducers(commandDialogNamespace),
--- src/views/issue/modal/issue.modal__attachment-actions-and-types.js
+++ src/views/issue/modal/issue.modal__attachment-actions-and-types.js
@@ -2,9 +2,9 @@
 
 import {attachmentActionMap, createAttachmentTypes} from 'components/attachments-row/attachment-helper';
 import {getAttachmentActions} from 'components/attachments-row/attachment-actions';
-import {singleIssueNamespace} from './issue.modal-reducers';
 
 import type {AttachmentActions} from 'components/attachments-row/attachment-actions';
 
-export const attachmentTypes: typeof attachmentActionMap = createAttachmentTypes(singleIssueNamespace);
-export const attachmentActions: AttachmentActions = getAttachmentActions(singleIssueNamespace);
+export const modalIssueNamespace = 'modal';
+export const attachmentTypes: typeof attachmentActionMap = createAttachmentTypes(modalIssueNamespace);
+export const attachmentActions: AttachmentActions = getAttachmentActions(modalIssueNamespace);
