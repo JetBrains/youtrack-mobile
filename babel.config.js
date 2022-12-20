c95fcb1d8a0169bdc3e9fb5e82a6239cd2385921
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2022-12-20 11:47:16 +0100

    YTM-19340 adjusted module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module-resolver', {
-      root: ['.', './src', '../..', '../../src'],
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      alias: {
-        actions: './src/actions',
-        assets: './src/assets',
-        components: './src/components',
-        flow: './src/flow',
-        util: './src/util',
-        views: './src/views',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.', './src', '../..', '../../src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          actions: './src/actions',
+          assets: './src/assets',
+          components: './src/components',
+          flow: './src/flow',
+          util: './src/util',
+          views: './src/views',
+        },
       },
-    }],
+    ],
     'react-native-reanimated/plugin',
     '@babel/plugin-transform-named-capturing-groups-regex',
   ],
