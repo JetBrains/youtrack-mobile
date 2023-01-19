2e3bd0dfbba9c55d58e19372b1d0ddbc4a6648bf
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-01-19 12:29:44 +0100

    YTM-19385 polishing code

--- src/views/article/article__edit-comment.tsx
+++ src/views/article/article__edit-comment.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+
 import {useDispatch} from 'react-redux';
+
 import IssueCommentEdit from 'components/comment/comment-edit';
 import IssuePermissions from 'components/issue-permissions/issue-permissions';
 import {attachmentActions} from './article__activity__attachment-actions-and-types';
 import {getApi} from 'components/api/api__instance';
 import {getMentions} from './arcticle-actions';
 import {visibilityArticleDefaultText} from 'components/visibility/visibility-strings';
+
 import type {Article} from 'types/Article';
 import type {Attachment, IssueComment} from 'types/CustomFields';
-type Props = {
+
+interface Props {
   article: Article;
   comment: IssueComment;
   issuePermissions: IssuePermissions;
-  onCommentChange?: (
-    comment: IssueComment,
-    isAttachmentChange: boolean,
-  ) => Promise<void> | void;
+  onCommentChange?: (comment: IssueComment, isAttachmentChange: boolean) => Promise<void> | void;
   onSubmitComment: (comment: IssueComment) => Promise<void>;
-};
+}
+
 
 const ArticleActivityStreamCommentEdit = (props: Props) => {
   const dispatch: (...args: any[]) => any = useDispatch();
@@ -28,21 +30,15 @@ const ArticleActivityStreamCommentEdit = (props: Props) => {
       isEditMode={true}
       onAttach={attachmentActions.uploadFileToArticleComment}
       onCommentChange={onCommentChange}
-      getVisibilityOptions={() =>
-        getApi().articles.getVisibilityOptions(article.id)
-      }
+      getVisibilityOptions={() => getApi().articles.getVisibilityOptions(article.id)}
       onSubmitComment={props.onSubmitComment}
       editingComment={props.comment}
       getCommentSuggestions={(query: string) => dispatch(getMentions(query))}
       canAttach={issuePermissions.articleCanAddAttachment(article)}
-      canRemoveAttach={(attachment: Attachment) =>
-        issuePermissions.articleCanDeleteAttachment(article)
-      }
+      canRemoveAttach={(attachment: Attachment) => issuePermissions.articleCanDeleteAttachment(article)}
       visibilityLabel={visibilityArticleDefaultText}
     />
   );
 };
 
-export default React.memo<Props>(
-  ArticleActivityStreamCommentEdit,
-) as React$AbstractComponent<Props, unknown>;
+export default React.memo<Props>(ArticleActivityStreamCommentEdit);
