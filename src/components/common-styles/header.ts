955feeeb0892b629e46fcedd9b10010ca5b92e4f
anisimov74 <anisimov74@gmail.com>
2023-01-31 17:28:32 +0100

    YTM-19385 removed unused code

--- src/components/common-styles/header.ts
+++ src/components/common-styles/header.ts
@@ -1,16 +1,3 @@
-import {Platform} from 'react-native';
 import {UNIT} from '../variables/variables';
-import {headerTitle} from './typography';
+
 export const menuHeight = UNIT * 8;
-export const headerTitleText = {
-  ...headerTitle,
-  color: '$text',
-  ...Platform.select({
-    ios: {
-      fontWeight: '500',
-    },
-    android: {
-      fontWeight: '600',
-    },
-  }),
-};
