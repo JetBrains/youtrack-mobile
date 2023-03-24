16e5e21ada85e95e3d5ab451da83e9aa42a3bc00
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-03-24 10:44:12 +0100

    chore: updated script for uploading Android source maps

--- script/bugsnag-upload-android.sh
+++ script/bugsnag-upload-android.sh
@@ -2,11 +2,18 @@
 
 set -ex
 
+VER="$2"
+IFS='.'
+read -a strarr <<< "$2"
+
+if [ "${strarr[2]}" -eq "0" ]; then
+  VER="${strarr[0]}.${strarr[1]}"
+fi
 
 echo "ANDROID: Source maps uploading..."
 bugsnag-source-maps upload-react-native \
   --api-key "$1" \
-  --app-version "$2" \
+  --app-version "$VER" \
   --platform android \
   --source-map dist/index.android.bundle.map \
   --bundle dist/index.android.bundle
