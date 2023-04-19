ab18d7e1483f9d19199857eb68dcb62572de4154
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-04-19 21:40:46 +0200

    chore: clean up

--- android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainApplication.java
+++ android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainApplication.java
@@ -5,12 +5,10 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -42,12 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new ReanimatedJSIModulePackage(); // <- add
-        }
-      };
+   };
 
   private final ReactNativeHost mNewArchitectureNativeHost =
       new MainApplicationReactNativeHost(this);
