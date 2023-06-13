7a87c9747ad4d8ec39a48bb06e2964f5fe0a93da
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-06-13 15:54:55 +0200

    #YTMR-230 fixed

---
+++ src/components/usage/usage.spec.ts
@@ -0,0 +1,83 @@
+// @ts-ignore
+import {Analytics, Hits} from 'react-native-google-analytics';
+
+import * as ga from './usage';
+import appPackage from '../../../package.json';
+
+const paramsMock = {};
+jest.mock('react-native-google-analytics', () => ({
+  Analytics: jest.fn().mockImplementation(() => ({
+    send: jest.fn().mockResolvedValue(paramsMock),
+  })),
+  Hits: {
+    ScreenView: jest.fn().mockReturnValue(paramsMock),
+    Event: jest.fn().mockReturnValue(paramsMock),
+  },
+}));
+
+
+describe('Google Analytics', () => {
+  beforeEach(() => {
+    ga.reset();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should create GA instance', async () => {
+    const instance: Analytics = await ga.getInstance();
+
+    await expect(instance.send).toBeTruthy();
+  });
+
+  it('should enable analytics', async () => {
+    expect(ga.isAnalyticsEnabled).toEqual(false);
+    ga.default.init(true);
+
+    expect(ga.isAnalyticsEnabled).toEqual(true);
+  });
+
+  it('should disable analytics', async () => {
+    ga.default.init(true);
+    expect(ga.isAnalyticsEnabled).toEqual(true);
+    ga.default.init(false);
+
+    expect(ga.isAnalyticsEnabled).toEqual(false);
+  });
+
+
+  describe('Analytics enabled', () => {
+    beforeEach(() => {
+      ga.default.init(true);
+    });
+
+    it('should create GA instance if it`s not created yet before sending an event', async () => {
+      expect(ga.gaAnalyticInstance).toEqual(null);
+      await ga.default.trackScreenView('');
+
+      await expect(ga.gaAnalyticInstance).toBeTruthy();
+    });
+
+    it('should track an event', async () => {
+      expect(ga.gaAnalyticInstance).toEqual(null);
+      await ga.default.trackEvent('');
+
+      await expect(ga.gaAnalyticInstance.send).toHaveBeenCalledWith(paramsMock);
+    });
+
+    it('should create screen view event params and track screen view', async () => {
+      const screenName: string = 'screenName';
+      await ga.default.trackScreenView(screenName);
+
+      await expect(Hits.ScreenView).toHaveBeenCalledWith(
+        'YouTrack Mobile',
+        screenName,
+        appPackage.version
+      );
+      await expect(ga.gaAnalyticInstance.send).toHaveBeenCalledWith(paramsMock);
+    });
+
+  });
+
+});
--- src/components/usage/usage.ts
+++ src/components/usage/usage.ts
@@ -1,6 +1,15 @@
 import DeviceInfo from 'react-native-device-info';
+// @ts-ignore
 import {Analytics, Hits as GAHits} from 'react-native-google-analytics';
+
 import appPackage from '../../../package.json';
+
+// @ts-ignore
+import ScreenView from 'react-native-google-analytics/lib/hits/ScreenView';
+// @ts-ignore
+import Event from 'react-native-google-analytics/lib/hits/Event';
+
+
 const splitRegExp = /[\.-]/i;
 const VERSION = appPackage.version || 'dev.dev.dev-dev';
 const [major, minor, patch, build] = VERSION.split(splitRegExp);
@@ -8,13 +17,14 @@ const patchPart = parseInt(patch) === 0 ? '' : `.${patch}`;
 export const VERSION_STRING: string = `${major}.${minor}${patchPart} (build ${build})`;
 export const USER_AGENT: string = `YouTrackMobile/${major}.${minor}${patchPart} (${DeviceInfo.getBrand()} ${DeviceInfo.getSystemName()} ${DeviceInfo.getSystemVersion()})`;
 const googleAnalyiticsId: string = appPackage.config.ANALYTICS_ID;
+
 let isAnalyticsEnabled: boolean = false;
-const clientId: string = DeviceInfo.getUniqueId();
-let gaAnalyticInstance: Analytics;
+let gaAnalyticInstance: Analytics | null = null;
 
 const createInstance = async () => {
+  const clientId: string = await DeviceInfo.getUniqueId();
   const userAgent: string = await DeviceInfo.getUserAgent();
-  gaAnalyticInstance = new Analytics(
+  return new Analytics(
     googleAnalyiticsId,
     clientId,
     1,
@@ -22,9 +32,30 @@ const createInstance = async () => {
   );
 };
 
-const getInstance = () => gaAnalyticInstance;
 
-createInstance();
+const reset = () => {
+  gaAnalyticInstance = null;
+};
+
+
+const getInstance = () => {
+  if (gaAnalyticInstance !== null) {
+    return gaAnalyticInstance;
+  }
+  return createInstance().then((instance: Analytics) => {
+    gaAnalyticInstance = instance;
+    return gaAnalyticInstance;
+  });
+};
+
+const send = async (param: ScreenView | Event): Promise<any> => {
+  const instance: Analytics = await getInstance();
+  if (instance?.send) {
+    return instance.send(param);
+  }
+  return Promise.reject(new Error('Analytics instance is not ready'));
+};
+
 const usage = {
   init(statisticsEnabled: boolean) {
     isAnalyticsEnabled = statisticsEnabled;
@@ -35,12 +66,12 @@ const usage = {
       return;
     }
 
-    const screenView = new GAHits.ScreenView(
+    const screenView: ScreenView = new GAHits.ScreenView(
       'YouTrack Mobile',
       screenName,
       VERSION,
     );
-    return getInstance().send(screenView);
+    return send(screenView);
   },
 
   trackEvent(eventName: string, ...params: any[]): any | void {
@@ -49,7 +80,7 @@ const usage = {
     }
 
     const gaEvent = new GAHits.Event(eventName, ...params);
-    return getInstance().send(gaEvent);
+    return send(gaEvent);
   },
 
   trackError(additionalMessage: string | null | undefined): any | void {
@@ -61,15 +92,15 @@ const usage = {
     );
   },
 
-  onGlobalError(error: any, isFatal: boolean): any | void {
-    return usage.trackError(
-      `Global error happened, isFatal:${isFatal.toString()}`,
-    );
-  },
 };
-const originalHandler = global.ErrorUtils.getGlobalHandler();
-global.ErrorUtils.setGlobalHandler((...params) => {
-  usage.onGlobalError(...params);
-  return originalHandler(...params);
-});
+
+
+export {
+  gaAnalyticInstance,
+  getInstance,
+  isAnalyticsEnabled,
+  reset,
+  send,
+};
+
 export default usage;
