ac283edac726f4a532ba283228dda9dc13828d55
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-09-14 12:04:38 +0200

    YTM-20996 adjust filterField values api call query

--- src/components/api/api__filter-fields.ts
+++ src/components/api/api__filter-fields.ts
@@ -1,6 +1,4 @@
 import ApiBase from './api__base';
-import ApiHelper from 'components/api/api__helper';
-import UserAPI from './api__user';
 
 import {FilterField, FilterFieldValue} from 'types/CustomFields';
 
@@ -8,23 +6,14 @@ import {FilterField, FilterFieldValue} from 'types/CustomFields';
 export default class FilterFields extends ApiBase {
 
   async filterFields(url: string, prefix: string = '', query: string = ''): Promise<any[]> {
-    const fields = ApiHelper.toField([
+    const fields = [
       'id',
       'presentation',
       'query',
       'name',
-    ]);
-    const queryString: string = UserAPI.createFieldsQuery(
-      fields,
-      {
-        $top: 100,
-        query,
-        prefix,
-        type: 'Issue',
-      }
-    );
+    ];
     return await this.makeAuthorizedRequest(
-      `${url}?${queryString}`,
+      `${url}?$%24top=100&fields=${fields.join(',')}&query=${encodeURIComponent(query)}&prefix=${encodeURIComponent(prefix)}&type=Issue`,
       'GET'
     );
   }
