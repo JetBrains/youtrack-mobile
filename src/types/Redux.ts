8d4e1815eb13911418a15ca7bcb6c61babe8053e
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2023-11-27 11:46:14 +0100

    chore: define types

--- src/types/Redux.ts
+++ src/types/Redux.ts
@@ -2,8 +2,12 @@ import Api from 'components/api/api';
 import {AppState} from 'reducers';
 
 
-export type ReduxAction = (
-  dispatch: (arg0: any) => any,
-  getState: () => AppState,
-  getApi: () => Api
-) => Promise<any>;
+export type ReduxActionDispatcher = (...args: any[]) => any;
+export type ReduxStateGetter = () => AppState;
+export type ReduxAPIGetter = () => Api;
+
+export type ReduxAction<T = void> = (
+  dispatch: ReduxActionDispatcher,
+  getState: ReduxStateGetter,
+  getApi: ReduxAPIGetter
+) => Promise<T>;
