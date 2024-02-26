dc9479721b4c8b31e314790e88006a2e091af26b
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2024-02-26 20:00:57 +0100

    YTM-21317 Settings icons

--- src/components/account/accounts.tsx
+++ src/components/account/accounts.tsx
@@ -10,10 +10,11 @@ import {
 import Swiper from 'react-native-swiper';
 
 import Avatar from 'components/avatar/avatar';
+import IconAdd from 'components/icon/assets/plus.svg';
+import IconLogout from 'components/icon/assets/logout.svg';
 import {formatYouTrackURL} from 'components/config/config';
 import {getStorageState} from 'components/storage/storage';
 import {HIT_SLOP} from 'components/common-styles/button';
-import {IconLogout, IconAdd} from 'components/icon/icon';
 
 import avatarStyles from 'components/avatar/default-avatar.styles';
 import styles, {SWIPER_HEIGHT} from './accounts.styles';
@@ -153,7 +154,7 @@ export default class Accounts extends PureComponent<Props, void> {
           disabled={isChangingAccount}
           onPress={onAddAccount}
         >
-          <IconAdd size={24} color={uiTheme.colors.$link} />
+          <IconAdd width={24} height={24} color={uiTheme.colors.$link} />
         </TouchableOpacity>
 
         {this.renderAccounts()}
@@ -167,7 +168,7 @@ export default class Accounts extends PureComponent<Props, void> {
           disabled={isChangingAccount}
           onPress={this._logOut}
         >
-          <IconLogout size={22} color={uiTheme.colors.$link} />
+          <IconLogout width={22} height={22} color={uiTheme.colors.$link} />
         </TouchableOpacity>
       </View>
     );
