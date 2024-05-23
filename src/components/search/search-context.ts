cd6d3f04f8766a47b2e5476708d88c7a7f93348a
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2024-05-23 15:22:27 +0200

    chore: translate a string

--- src/components/search/search-context.ts
+++ src/components/search/search-context.ts
@@ -1,7 +1,8 @@
 import type {Folder} from 'types/User';
+import {i18n} from 'components/i18n/i18n';
 
 export const EVERYTHING_SEARCH_CONTEXT: Folder = {
   id: null,
-  name: 'Everything',
+  name: i18n('Everything'),
   query: '',
 } as unknown as Folder;
