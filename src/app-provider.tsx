41af5a92db7a003f5397c0fb40e53b73bde2d357
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2024-11-12 14:51:44 +0100

    refactor: migrate to func component

--- src/app-provider.tsx
+++ src/app-provider.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {StatusBar} from 'react-native';
 
 import {Host} from 'react-native-portalize';
@@ -24,59 +24,55 @@ import {ThemeContext} from 'components/theme/theme-context';
 import type {Theme, UITheme} from 'types/Theme';
 import type {ViewStyleProp} from 'types/Internal';
 
-type State = { mode: string; } | undefined;
 
-export default class AppProvider extends Component<void, State> {
-  state: State;
+const AppProvider = () => {
+  const [mode, setMode] = React.useState<string | undefined>();
 
-  async UNSAFE_componentWillMount() {
-    const themeMode: string = await getThemeMode();
-    buildStyles(themeMode, getUITheme(themeMode));
-    this.setState({
-      mode: themeMode,
-    });
-  }
+  React.useEffect(() => {
+    const fetchThemeMode = async () => {
+      const themeMode: string = await getThemeMode();
+      buildStyles(themeMode, getUITheme(themeMode));
+      setMode(themeMode);
+    };
+    fetchThemeMode();
+  }, []);
 
-  render(): React.ReactNode {
-    if (!this?.state?.mode) {
-      return null;
-    }
+  return !mode ? null : (
+    <ThemeProvider mode={mode}>
+      <ThemeContext.Consumer>
+        {(theme: Theme) => {
+          const uiTheme: UITheme = theme.uiTheme || DEFAULT_THEME;
+          const flexStyle: Partial<ViewStyleProp> = {flex: 1};
+          const backgroundStyle: Partial<ViewStyleProp> = {backgroundColor: uiTheme.colors.$background};
+          return (
+            <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+              <StatusBar
+                backgroundColor={backgroundStyle.backgroundColor}
+                barStyle={uiTheme.barStyle}
+                translucent={true}
+              />
+              <SafeAreaView style={[flexStyle, backgroundStyle]}>
+                <ErrorBoundary>
+                  <Host>
+                    <Navigation/>
+                    <UserAgreement/>
+                    <DebugView
+                      logsStyle={{
+                        ...backgroundStyle,
+                        textColor: uiTheme.colors.$text,
+                        separatorColor: uiTheme.colors.$separator,
+                      }}
+                    />
+                  </Host>
+                </ErrorBoundary>
+                <Network/>
+              </SafeAreaView>
+            </SafeAreaProvider>
+          );
+        }}
+      </ThemeContext.Consumer>
+    </ThemeProvider>
+  );
+};
 
-    return (
-      <ThemeProvider mode={this.state.mode}>
-        <ThemeContext.Consumer>
-          {(theme: Theme) => {
-            const uiTheme: UITheme = theme.uiTheme || DEFAULT_THEME;
-            const flexStyle: Partial<ViewStyleProp> = {flex: 1};
-            const backgroundStyle: Partial<ViewStyleProp> = {backgroundColor: uiTheme.colors.$background};
-            return (
-              <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-                <StatusBar
-                  backgroundColor={backgroundStyle.backgroundColor}
-                  barStyle={uiTheme.barStyle}
-                  translucent={true}
-                />
-                <SafeAreaView style={[flexStyle, backgroundStyle]}>
-                  <ErrorBoundary>
-                    <Host>
-                      <Navigation/>
-                      <UserAgreement/>
-                      <DebugView
-                        logsStyle={{
-                          ...backgroundStyle,
-                          textColor: uiTheme.colors.$text,
-                          separatorColor: uiTheme.colors.$separator,
-                        }}
-                      />
-                    </Host>
-                  </ErrorBoundary>
-                  <Network/>
-                </SafeAreaView>
-              </SafeAreaProvider>
-            );
-          }}
-        </ThemeContext.Consumer>
-      </ThemeProvider>
-    );
-  }
-}
+export default AppProvider;
