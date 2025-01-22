58b64a6c8292a691c9d649c7f447b08e8b0715f7
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2025-01-22 13:02:02 +0100

    infra: prevent commit if ignored all logs

--- script/test-dev-data.js
+++ script/test-dev-data.js
@@ -1,10 +1,12 @@
-const pkg = require('../package.json'); //eslint-disable-line
-
+const pkg = require('../package.json');
+const fg = require('fast-glob');
+const fs = require('fs');
 
 if (hasExceptionReporterToken() || hasAnalyticsId() || hasKonnektorData() || hasBugsnagData()) {
   throw new Error('`Config` or `Bugsnag Config` or `Google Services` is touched. Revert changes and proceed.');
 }
 
+checkIfLogsIgnored();
 
 function hasExceptionReporterToken() {
   return isNotEmpty(pkg.config.EXCEPTION_REPORTER_TOKEN);
@@ -24,4 +26,19 @@ function hasBugsnagData() {
 
 function isNotEmpty(param) {
   return param.trim().length > 0;
+}
+
+function checkIfLogsIgnored() {
+  const filePath = './src/app.tsx';
+  const searchText = 'LogBox.ignoreAllLogs()';
+
+  fg([filePath]).then((files) => {
+    if (files.length === 0) {
+      throw new Error(`❌ File "${filePath}" not found.`);
+    }
+    const fileContent = fs.readFileSync(files[0], 'utf8');
+    if (fileContent.includes(searchText)) {
+      throw new Error(`❌ The file "${filePath}" contains the text: "${searchText}".`);
+    }
+  });
 }
