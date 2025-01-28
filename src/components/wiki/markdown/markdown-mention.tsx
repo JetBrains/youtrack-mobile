5dccf10435e76e50c2be9a855eec24a09b052c11
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2025-01-28 13:15:33 +0100

    refactor: clean up; fixed types

--- src/components/wiki/markdown/markdown-mention.tsx
+++ src/components/wiki/markdown/markdown-mention.tsx
@@ -14,43 +14,63 @@ import type {User} from 'types/User';
 
 import styles from 'components/wiki/markdown/markdown.styles';
 
+const getLinkStyle = (m: Mention) => {
+  if ('resolved' in m) {
+    return styles.resolved;
+  }
+  return styles.link;
+};
 
-export default function MarkdownMention({mention, onPress, style}: {
-  mention: string,
-  onPress: () => void,
-  style: TextStyle | TextStyle[],
-}): JSX.Element | null {
+export default function MarkdownMention({
+  mention,
+  onPress,
+  style,
+}: {
+  mention: string;
+  onPress: () => void;
+  style: TextStyle | TextStyle[];
+}): React.ReactNode {
   return (
-    <Text
-      key={guid()}
-      onPress={onPress}
-      selectable={true}
-      style={style}
-    >
+    <Text key={guid()} onPress={onPress} selectable={true} style={style}>
       {mention}
     </Text>
   );
 }
 
-
-export function MarkdownMentionWithUserCard({mention, style}: {
-  mention: Mention,
-  style: TextStyle | TextStyle[]
-}): JSX.Element | null {
+export function MarkdownMentionWithUserCard({
+  mention,
+  style,
+}: {
+  mention: Mention;
+  style: TextStyle | TextStyle[];
+}): React.ReactNode {
   const {openBottomSheet} = useBottomSheetContext();
 
-  const getMentionPresentation = (mention: Mention): string => {
-    return hasType.user(mention) ? `@${getEntityPresentation(mention)}` : mention.summary || mention.idReadable;
+  const getMentionIcon = (m: Mention) => {
+    const color: string = getLinkStyle(m).color;
+    if (hasType.article(m)) {
+      return (
+        <Text>
+          <IconFileText size={15} color={color} />
+          {'\u00A0'}
+        </Text>
+      );
+    } else {
+      return hasType.issue(m) ? <IconFileCheck size={18} color={color} /> : null;
+    }
   };
 
-  const getLinkStyle = (mention: Mention) => {
-    return mention.resolved ? styles.resolved : styles.link;
-  };
-
-  const getMentionIcon = (mention: Mention) => {
-    return hasType.article(mention)
-      ? <Text><IconFileText size={15} color={getLinkStyle(mention).color}/>{'\u00A0'}</Text>
-      : hasType.issue(mention) ? <IconFileCheck size={18} color={getLinkStyle(mention).color}/> : null;
+  const getMentionPresentation = (m: Mention): string => {
+    if (hasType.user(m)) {
+      return `@${getEntityPresentation(m)}`;
+    }
+    if ('summary' in m) {
+      return m.summary;
+    }
+    if ('idReadable' in m) {
+      return m.idReadable;
+    }
+    return '';
   };
 
   return (
@@ -64,14 +84,15 @@ export function MarkdownMentionWithUserCard({mention, style}: {
         if (hasType.user(mention)) {
           openBottomSheet({
             withHandle: false,
-            children: <UserCard user={mention as User}/>,
+            children: <UserCard user={mention as User} />,
           });
         }
       }}
       selectable={true}
       style={[style, getLinkStyle(mention)]}
     >
-      {getMentionIcon(mention)}{getMentionPresentation(mention)}
+      {getMentionIcon(mention)}
+      {getMentionPresentation(mention)}
     </Text>
   );
 }
