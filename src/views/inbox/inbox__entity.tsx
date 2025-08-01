2e36ddb9141a11bf34474be65d8b76c499dbf60c
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2025-08-01 14:29:35 +0200

    refactor: adjust types and conditional checks

--- src/views/inbox/inbox__entity.tsx
+++ src/views/inbox/inbox__entity.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
+
 import {HIT_SLOP} from 'components/common-styles';
 import {getReadableID} from 'components/issue-formatter/issue-formatter';
+
 import styles from './inbox.styles';
+
 import type {AnyIssue} from 'types/Issue';
 import type {Article} from 'types/Article';
 import type {TextStyleProp} from 'types/Internal';
-export default function InboxEntity({
-  entity,
-  onNavigate,
-  styleText,
-  ...otherProps
-}: {
+import type {TouchableWithoutFeedbackProps} from 'react-native';
+
+interface Props extends TouchableWithoutFeedbackProps {
   entity: AnyIssue | Article;
   onNavigate: () => void;
   styleText?: TextStyleProp;
-}) {
-  const readableID: string | null | undefined = getReadableID(entity);
+}
+
+export default function InboxEntity({entity, onNavigate, styleText, ...otherProps}: Props) {
+  const readableID = getReadableID(entity);
   return (
     <TouchableOpacity hitSlop={HIT_SLOP} onPress={onNavigate} {...otherProps}>
       <Text numberOfLines={1}>
@@ -28,13 +30,13 @@ export default function InboxEntity({
             style={[
               styles.notificationIssueInfo,
               styleText,
-              entity.resolved ? styles.resolved : null,
+              'resolved' in entity && entity.resolved ? styles.resolved : null,
             ]}
           >
             {readableID}
           </Text>
         )}
-        {!!entity.summary && (
+        {!!('summary' in entity && entity.summary) && (
           <Text
             testID="test:id/inboxEntitySummary"
             accessibilityLabel="inboxEntitySummary"
