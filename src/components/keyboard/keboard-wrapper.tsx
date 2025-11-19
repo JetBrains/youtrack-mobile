ec8a4575344d05aaa2b2cdaf238490267ec764c5
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2025-11-19 17:22:13 +0100

    YTM-22066 use same `behavior` for both platforms

--- src/components/keyboard/keboard-wrapper.tsx
+++ src/components/keyboard/keboard-wrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {KeyboardAvoidingView, Platform} from 'react-native';
+import {KeyboardAvoidingView} from 'react-native';
 
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {ViewStyleProp} from 'types/Internal';
@@ -16,17 +16,12 @@ const styles = {flex: 1} as const;
 export const KeyboardWrapper = (props: Props) => {
   const {verticalOffset = 0, style, isInModal = false} = props;
   const insets = useSafeAreaInsets();
-  const calculatedOffset = isInModal && Platform.OS === 'ios'
+  const calculatedOffset = isInModal
     ? verticalOffset + insets.top
     : verticalOffset;
 
   return (
-    <KeyboardAvoidingView
-      style={[styles, style]}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      enabled
-      keyboardVerticalOffset={calculatedOffset}
-    >
+    <KeyboardAvoidingView style={[styles, style]} behavior="padding" enabled keyboardVerticalOffset={calculatedOffset}>
       {props.children}
     </KeyboardAvoidingView>
   );
