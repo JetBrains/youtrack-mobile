b3c38b3f3a081e10f6ccdb93b4347f7ab0fa9817
Alexander Anisimov <alexander.anisimov@jetbrains.com>
2026-01-13 21:15:57 +0100

    fix(Android): add missing onCreate override in MainActivity

--- android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainActivity.kt
+++ android/app/src/main/java/com/jetbrains/youtrack/mobile/app/MainActivity.kt
@@ -15,6 +15,10 @@ class MainActivity : ReactActivity() {
      */
     override fun getMainComponentName(): String = "YouTrackMobile"
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
+
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
      * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
